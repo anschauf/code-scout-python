23d8b423aac48214f5b5356d2003fab4469a538e
liruizhang <lirui.zhang@aimedic.ch>
2022-08-19 10:56:35 +0200

    [2022-32] [WIP] drafted performance app

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from os.path import basename, splitext
 
 import awswrangler as wr
+import pandas as pd
 from matplotlib import pyplot as plt
 
 s3_dir_rankings = 's3://code-scout/performance-measuring/mock_rankings/'
@@ -18,44 +19,93 @@ for filename in all_ranking_filenames:
 
 code_ranks = []
 
-
 for method_name, rankings in all_rankings:
 
     rankings['suggested_codes_pdx_split'] = rankings['suggested_codes_pdx'].apply(lambda x: x.split('|') if (isinstance(x, str)) else [])
-    revised_cases['ICD_added_split'] =  revised_cases['ICD_added'].apply(lambda x: x.split('|') if (isinstance(x, str)) else [])
+
+    revised_cases['ICD_added_split'] = revised_cases['ICD_added'].apply(lambda x: x.split('|') if (isinstance(x, str)) else [])
 
     for case_index in range(revised_cases.shape[0]):
         current_case = revised_cases.iloc[case_index]
         case_Id = current_case['CaseId']
-        ICD_added_list = current_case['ICD_added_split']# AttributeError: 'float' object has no attribute 'split'
-        # ICD_added = str(ICD_added)
-        #ICD_added_list = ICD_added.split('|')
-        case_model = rankings[rankings['case_id'] == case_Id]
-        ICD_suggested_list = case_model['suggested_codes_pdx_split']
-
+        ICD_added_list = current_case['ICD_added_split']
+        ranking_case_id = rankings['case_id'].tolist()
 
         # find matching case id in rankings if present
         # if not present, skip
+        if case_Id in ranking_case_id:
+            ICD_suggested_list = rankings[rankings['case_id'] == case_Id]['suggested_codes_pdx_split'].values[0]
+        else:
+            continue
+
+
 
         # if present split icd_added field in current_case object into single diagnoses, e.g. K5722|E870 => ['K5722',  'E870']
         # use .split('|')
 
         # find revised diagnoses in current ranking after also here splitting the diagnoses like before with the revised case
+        # if diagnosis is present, find index where its ranked and classify it in one of the ranking labels
+        # 1-3, 4-6, 7-9, 10+
+        # if not present add to not suggested label
         for ICD in ICD_added_list:
             if ICD not in ICD_suggested_list:
                 rank = 'not suggest'
             else:
                 idx = ICD_added_list.index(ICD)
                 rank = idx + 1
-            code_rank = [case_Id, method_name, rank]
-            code_ranks.append(code_rank)
 
-        # if diagnosis is present, find index where its ranked and classify it in one of the ranking labels
-        # 1-3, 4-6, 7-9, 10+
-        # if not present add to not suggested label
+        if rank in [1,2,3]:
+                rank_1_3 = 1
+                rank_4_6 = 0
+                rank_7_9 = 0
+                rank_10 = 0
+                rank_not_suggest = 0
+        elif rank in [4, 5, 6]:
+                rank_1_3 = 0
+                rank_4_6 = 1
+                rank_7_9 = 0
+                rank_10 = 0
+                rank_not_suggest = 0
+        elif rank in [7, 8, 9]:
+                rank_1_3 = 0
+                rank_4_6 = 0
+                rank_7_9 = 1
+                rank_10 = 0
+                rank_not_suggest = 0
+        elif rank == 'not suggest':
+                rank_1_3 = 0
+                rank_4_6 = 0
+                rank_7_9 = 0
+                rank_10 = 0
+                rank_not_suggest = 1
+        else:
+            rank_1_3 = 0
+            rank_4_6 = 0
+            rank_7_9 = 0
+            rank_10 = 1
+            rank_not_suggest = 0
+
+        code_rank = [case_Id, ICD, ICD_suggested_list, method_name, rank_1_3, rank_4_6, rank_7_9, rank_10, rank_not_suggest ]
+        code_ranks.append(code_rank)
+
 
         # store in an object
         # case id, revised_icd, method name, 5 ranking labels
+import pandas as pd
+rank_evaluation = pd.DataFrame(code_ranks, columns=['case_id', 'added_ICD', 'ICD_suggested_list', 'method_name',  'rank_1_3', 'rank_4_6', 'rank_7_9', 'rank_10', 'rank_not_suggest'])
+
+
+rank_evaluation_drg_1 = rank_evaluation.loc[rank_evaluation['method_name'] == 'DRG-tree-1-prototype']
+rank_evaluation_drg_2 = rank_evaluation.loc[rank_evaluation['method_name'] == 'DRG-tree-2-prototype']
+
+drg1_rank_sum = rank_evaluation_drg_1.loc[:, ['rank_1_3', 'rank_4_6', 'rank_7_9', 'rank_10', 'rank_not_suggest']].sum(axis=0)
+drg2_rank_sum = rank_evaluation_drg_2.loc[:, ['rank_1_3', 'rank_4_6', 'rank_7_9', 'rank_10', 'rank_not_suggest']].sum(axis=0)
+
+
+
+# same result in the local folder
+rank_evaluation.to_csv('rank_evaluation.csv')
+
 
 
 # plt.figure()
@@ -68,8 +118,6 @@ for method_name, rankings in all_rankings:
 # plt.tight_layout()
 # plt.savefig(join(dir_output, f'bar_ranking_classes{tag}.pdf'), bbox_inches='tight')
 # plt.close()
-
-
 
 
 
