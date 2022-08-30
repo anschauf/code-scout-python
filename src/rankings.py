7aabe62f21540e894a1a2e55589870b0287d2ab5
liruizhang <lirui.zhang@aimedic.ch>
2022-08-30 11:52:23 +0200

    [2022-32] Modification of ranking labels and ranking ranges allow the flexibility

--- src/rankings.py
+++ src/rankings.py
@@ -1,3 +1,3 @@
-LABEL_NOT_SUGGESTED = 'not suggest'
+LABEL_NOT_SUGGESTED = 99999
 RANKING_LABELS = ['rank_1_3', 'rank_4_6', 'rank_7_9', 'rank_10', 'rank_not_suggest']
-
+RANKING_RANGES = [1, 4, 7, 10, 99999]
