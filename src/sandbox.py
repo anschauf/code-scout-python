8e4fd1276ca65a1c54edccfdd604be1aaa532aaf
michael_kunz <michael.kunz@aimedic.ch>
2022-09-08 13:52:59 +0200

    "[2022-36] [WIP] added sandbox file to start examining datasets

--- src/sandbox.py
+++ src/sandbox.py
@@ -1,125 +1,147 @@
-import numpy as np
-import venn
-from src.files import load_revised_cases, load_code_scout_results, load_all_rankings
+mport os
+from os.path import basename, splitext
 
-
-git clone https://github.com/tctianchi/pyvenn.git
-cd pyvenn
-
-from pyvenn import venn
-
+import awswrangler as wr
 import matplotlib
-matplotlib.use('Agg')
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from loguru import logger
+from src import venn
 
-import venn
-cd pyvenn
 
 
-In [5]: labels = venn.get_labels([
-            range(10),
-            range(5, 15)
-        ], fill=['number', 'logic'])
 
-fig, ax = venn(labels, names=['list 1', 'list 2'])
-fig.show()
+def load_revised_cases(filename_revised_cases: str) -> pd.DataFrame:
+    logger.info(f'Reading revised cases from {filename_revised_cases} ...')
+    revised_cases = wr.s3.read_csv(filename_revised_cases)
+    logger.info(f'Read {revised_cases.shape[0]} rows')
+
+    return revised_cases
+
+def load_code_scout_results(dir_rankings: str) -> list[(str, pd.DataFrame)]:
+    # load rankings and store them in a tuple
+    logger.info(f'Listing files in {dir_rankings} ...')
+    all_ranking_filenames = wr.s3.list_objects(dir_rankings)
+    if len(all_ranking_filenames) == 0:
+        raise Exception(f'Found no ranking files')
+    else:
+        logger.info(f'Found {len(all_ranking_filenames)} files')
+
+    all_rankings = list()
+    for filename in all_ranking_filenames:
+        logger.info(f'Reading {filename} ...')
+        rankings = wr.s3.read_csv(filename)
+
+        method_name = splitext(basename(filename))[0]
+        hospital_year = os.path.dirname(filename).split('/')[-1]
+        all_rankings.append((hospital_year, method_name, rankings))
+
+    return all_rankings
+
+print("")
 
 
 def create_rankings_of_revised_cases(
         filename_revised_cases: str,
-        filename_codescout_results: str,
-        dir_rankings: str
-    ):
+        filename_codescout_results: str,):
 
     # load revision data from DtoD
     revised_cases = load_revised_cases(filename_revised_cases)
+    # load the KSW2019 data from Codescout
+    codescout_rankings = load_code_scout_results(filename_codescout_results)
+    revised_cases['CaseId'] = revised_cases['FID']
+    revised_cases['CaseId'] = revised_cases['CaseId'].fillna(0).astype(int)
 
-    # Check if revision data from DtoD has KSW 2019 cases (to check if there are matching case_ids with the revision files)
-    KSW = revised_cases[(revised_cases['Year'] == 2020) & (revised_cases['Clinic'] == 'KSW')]
+    for hospital_year, method_name, rankings in codescout_rankings:
+        # sort the codescout_rankings based on probabilities and get the caseID from codescout_rankings as list
+        rankings.sort_values(by='prob_most_likely_code', ascending=False)
+        caseid_codescout = rankings['case_id'].tolist()
 
-    # renaming 'CaseId' of revision data to 'case_id' to comply with codescout rankings dataset
-    revised_cases.rename(columns={'CaseId':'case_id'}, inplace =True)
+        # get the caseid from revised cases as a list
+        caseid_revised = revised_cases['CaseId'].tolist()
 
-
-    # load the KSW19 data from Codescout and extract necessary dataframe
-    codescout_rankings_ksw = load_code_scout_results(filename_codescout_results)
-    codescout_rankings_ksw_df = codescout_rankings_ksw[0][1]
-
-    # sort the codescout_rankings based on probabilities
-
-    codescout_rankings_ksw_df.sort_values(by = 'prob_most_likely_code', ascending = False)
-
-    # make a column with a probability rank to see how high each case ranks in code scout for the comparison with DtoD
-    codescout_rankings_ksw_df['probability_rank'] = codescout_rankings_ksw_df.index + 1
-
-    # if two cases (two identical CaseID) present in codescout suggestions -> ignore the case at the moment
-    # !! probably not necessary as a duplicate function is integrated in the files function
-    codescout_rankings_ksw_df.drop_duplicates(subset="case_id", keep=False)
-
-    # (and get the caseID from codescout_rankings as list)
-    case_id_codescout_list = list(codescout_rankings_ksw_df['case_id'])
-    revised_cases_list = list(revised_cases['case_id'])
-
-    print("")
-
-    # Quick check to see if any case_id from codescout appears in DtoD file
-    revised_cases_ksw = revised_cases[revised_cases['case_id'].isin([case_id_codescout_list])]
-
-    mind_bend_drg_tree = load_code_scout_results(filename_mind_bend)
-
-
-
-    # to be modified and added later
-
-    def list_contains(revised_cases_list, revised_cases_ksw):
-        check = False
-        # Iterate in the 1st list
-        for item1 in revised_cases_list:
-            # Iterate in the 2nd list
-            for item2 in revised_cases_ksw:
-                # if there is a match
-                if item1 == item2:
-                    check = True
-                    return check
-        return check
-
-
-    check = any(item in revised_cases_list for item in revised_cases_kssg)
-
-    if check:
-        print("The revised_cases_list contains some elements of the revised_cases_kssg")
-    else:
-        print("No, revised_cases_list doesn't have any elements of the revised_cases_kssg.")
-
-    list_contains(revised_cases_list, revised_cases_kssg)
-
-
-
-    # go to revision cases
-    # based on caseID get the index (or 'caserank_codescout') from the Codescout data
-
-    # (iterate through revision cases (for loop) doing this:
-
-        # for case_id in revised_cases):
-
+        # go to revision cases
+        # based on caseID get the index from the Codescout data
+        # iterate through revision cases (for loop) doing this:
         #   check if caseID (of DtoD revision data) is present in the Codescout suggestions data
         #   if yes, return the row index (i.e. position or rank bucket)
+        # if two cases (two identical CaseID) present in Codescout suggestions -> ignore the case at the moment
+        revised_codescout_overlap = list()
+        for case in caseid_revised:
+            if case in caseid_codescout and case != 0:
+                revised_codescout_overlap.append(caseid_codescout.index(case))
+        # check the venn diagram (if possible, so we can skip making a label with rank groups) (pyvenn or venn)
+        # Count the number of row index fall into different rank tiers
+        # venn diagram
+        # https://github.com/tctianchi/pyvenn
 
-    # check the venn diagram (if possible, so we can skip making a label with rank groups) (pyvenn or venn)
-    # Count the number of row index fall into different rank tiers
+        top100 = set(np.arange(0, 100))
+        top1000 = set(np.arange(0, 1000))
+        all_cases = set(np.arange(len(caseid_codescout)))
+        revised_codescout_overlap = set(revised_codescout_overlap)
+
+        labels = venn.get_labels([top100, top1000, all_cases, revised_codescout_overlap],
+                                 fill=['number', 'logic'])
+        fig, ax = venn.venn4(labels, names=['Top 100', 'Top 1000', 'All cases', 'Revised cases'])
+        fig.suptitle(f'Case Ranking Tier ({hospital_year})', fontsize=40)
+        fig.savefig(f'case_ranking_{hospital_year}.png', bbox_inches='tight')
+        fig.show()
 
 
 
 
-    # venn diagram
-    # https://github.com/tctianchi/pyvenn
 
+filename_revised_cases="s3://code-scout/performance-measuring/CodeScout_GroundTruthforPerformanceMeasuring.csv"
+filename_codescout_results="s3://code-scout/performance-measuring/case_rankings/DRG_tree/revisions/ksw2019/"
+
+
+#def calculate_cumulative_distribution(
+#        filename_revised_cases: str,
+#        filename_codescout_results: str,):
+
+# load revision data from DtoD
+revised_cases = load_revised_cases(filename_revised_cases)
+# load the KSW2019 data from Codescout
+codescout_rankings = load_code_scout_results(filename_codescout_results)
+revised_cases['CaseId'] = revised_cases['FID']
+revised_cases['CaseId'] = revised_cases['CaseId'].fillna(0).astype(int)
+
+
+
+# fixing dataformat errors in order to convert CW_old and CW_new to floats and calculate delta_cw
+revised_cases['CW_old'] = revised_cases['CW_old'].str.replace(',', '.').astype(float)
+
+revised_cases['CW_new'] = revised_cases['CW_new'].str.replace('-', '0').astype(float)
+# replace 0 with value from same index in previous column
+
+
+revised_cases['delta_cw'] = revised_cases['CW_new'] - revised_cases['CW_old']
+
+
+cw_new_list = revised_cases['CW_new'].tolist()
+cw_old_list = revised_cases['CW_old'].tolist()
+
+delta_cw = list()
+
+for item1, item2 in zip(cw_new_list, cw_old_list):
+    item = item1 - item2
+    delta_cw.append(item)
+
+
+
+
+
+
+
+
+
+
+
+filename_revised_cases="s3://code-scout/performance-measuring/CodeScout_GroundTruthforPerformanceMeasuring.csv"
+filename_codescout_results="s3://code-scout/performance-measuring/case_rankings/DRG_tree/revisions/ksw2019/"
 
 if __name__ == '__main__':
     create_rankings_of_revised_cases(
-        dir_rankings='s3://code-scout/performance-measuring/code_rankings/mock_rankings/',
-        filename_revised_cases="s3://code-scout/performance-measuring/revised_evaluation_cases.csv",
-        filename_codescout_results="s3://code-scout/performance-measuring/case_rankings/DRG_tree/revisions/ksw2019/",
-        filename_mindbend ="s3://code-scout/performance-measuring/code_rankings/2022-08-24_mindbend-drg-tree/drg-tree-revised-cases-total.csv"
-    )
-
-
+        filename_revised_cases="s3://code-scout/performance-measuring/CodeScout_GroundTruthforPerformanceMeasuring.csv",
+        filename_codescout_results="s3://code-scout/performance-measuring/case_rankings/DRG_tree/revisions/ksw2019/")
