b53651c8c066929f100458a501fb2307c0ea1882
liruizhang <lirui.zhang@aimedic.ch>
2022-10-11 17:10:24 +0200

    [2022-40] Create the jupyter notebooks for the sheets in the excel HI-Bern_Salem_Beau Site_Linde

--- src/revised_case_normalization/py/global_configs.py
+++ src/revised_case_normalization/py/global_configs.py
@@ -18,64 +18,41 @@ FILES_TO_ANALYZE = {
          os.path.join(ROOT_DIR, 'raw_data/HI-Bern_Salem_Beau Site_Linde.xlsx'),
          'Hirslanden Salem',
          '2017',
-         ['Änderungen_SA_2017'],
+         ['Änderungen_SA_2017']),
     
     'Hirslanden Beau Site 2017': FileInfo(
          os.path.join(ROOT_DIR, 'raw_data/HI-Bern_Salem_Beau Site_Linde.xlsx'),
          'Hirslanden Beau Site',
           '2017',
-         ['Änderungen Beau Site 2017', 'Änderungen_LI_2017', 'Änderungen_LI_2018', 'Änderungen _SA_2018', 'Änderungen Beau Site_ 2018']),
+         ['Änderungen Beau Site 2017']),
+        
     
-    'HI-Bern_Salem_Beau Site_Linde.xlsx': FileInfo(
+    'Hirslanden Linde 2017': FileInfo(
          os.path.join(ROOT_DIR, 'raw_data/HI-Bern_Salem_Beau Site_Linde.xlsx'),
-         ['SA', 'Hirslanden Beau Site', 'Hirslanden Linde', 'Hirslanden Linde', 'SA', 'Hirslanden Beau Site'],
-         ['2017', '2017', '2017', '2018', '2018', '2018'],
-         ['Änderungen_SA_2017', 'Änderungen Beau Site 2017', 'Änderungen_LI_2017', 'Änderungen_LI_2018', 'Änderungen _SA_2018', 'Änderungen Beau Site_ 2018']),
+         'Hirslanden Linde',
+          '2017',
+         ['Änderungen_LI_2017']),
     
-    'HI-Bern_Salem_Beau Site_Linde.xlsx': FileInfo(
+    'Hirslanden Linde 2018': FileInfo(
          os.path.join(ROOT_DIR, 'raw_data/HI-Bern_Salem_Beau Site_Linde.xlsx'),
-         ['SA', 'Hirslanden Beau Site', 'Hirslanden Linde', 'Hirslanden Linde', 'SA', 'Hirslanden Beau Site'],
-         ['2017', '2017', '2017', '2018', '2018', '2018'],
-         ['Änderungen_SA_2017', 'Änderungen Beau Site 2017', 'Änderungen_LI_2017', 'Änderungen_LI_2018', 'Änderungen _SA_2018', 'Änderungen Beau Site_ 2018']),
+         'Hirslanden Linde',
+         '2018',
+         ['Änderungen_LI_2018']),
+    
+    'Hirslanden Salem 2018': FileInfo(
+         os.path.join(ROOT_DIR, 'raw_data/HI-Bern_Salem_Beau Site_Linde.xlsx'),
+         'Hirslanden Salem',
+         '2018',
+         ['Änderungen _SA_2018']),
+        
+    'Hirslanden Beau Site 2018': FileInfo(
+         os.path.join(ROOT_DIR, 'raw_data/HI-Bern_Salem_Beau Site_Linde.xlsx'),
+         'Hirslanden Beau Site',
+         '2018',
+         ['Änderungen Beau Site_ 2018']),
     
     
 
-    'HI-Zurich.xlsx': FileInfo(
-         os.path.join(ROOT_DIR, 'raw_data/HI-Zurich.xlsx'),
-         ['Hirslanden Zurich', 'Hirslanden Zurich', 'Hirslanden Zurich', 'Hirslanden Zurich'],
-         ['2016', '2017', '2018', '2019'],
-         ['Änderungen_Hirslanden 2016', 'Änderungen_Hirslanden_2017', 'Änderungen_Hirslanden_2018', 'Änderungen_MIX_Hirslanden_2019']),
-
-    'HI_Aarau_Birshof_ST. Anna.xlsx': FileInfo(
-        os.path.join(ROOT_DIR, 'raw_data/HI_Aarau_Birshof_ST. Anna.xlsx'),
-        ['Hirslanden St. Anna', 'Hirslanden Aarau', 'Hirslanden Birshof', 'Hirslanden Birshof', 'Hirslanden Aarau', 'Hirslanden St. Anna'],
-        ['2017', '2017', '2017', '2018', '2018', '2018'],
-        ['Änderungen_ST. Anna_2017', 'Aarau_2017', 'Birshof_2017', 'Birshof_2018', 'Aarau 2018',
-         'Änderungen_ST. Anna_2018']),
-
-    'KSSG_2021.xlsx': FileInfo(os.path.join(ROOT_DIR, 'raw_data/KSSG_2021.xlsx'),
-                          ['KSSG HQ'], ['2021'], ['Änderungen_KSSG_2021']),
-
-    'Linth_Toggenburg_SRRWS_2019.xlsx': FileInfo(
-         os.path.join(ROOT_DIR, 'raw_data/Linth_Toggenburg_SRRWS_2019.xlsx'),
-         ['KSSG Linth', 'KSSG Toggenburg', 'KSSG SRRWS'],
-         ['2019', '2019', '2019'],
-         ['Änderungen_Spital_Linth_2019', 'Änderungen_Toggenburg_2019', 'Änderungen SRRWS_2019']),
-
-    'USZ_2018-2019_20200730.xlsx': FileInfo(
-         os.path.join(ROOT_DIR, 'raw_data/USZ_2018-2019_20200730.xlsx'),
-         ['USZ', 'USZ', 'USZ'],
-         ['2018', '2019', '2019-2020'],
-         ['Rückmeldungen_USZ_2018', 'Gesamtauffällige_USZ_2019', 'Rückmeldung_USZ_2019_30.04.2020']),
-
-    'Winterthur.xlsx': FileInfo(
-         os.path.join(ROOT_DIR, 'raw_data/Winterthur.xlsx'),
-         ['KSW',  'KSW'],
-         ['2017',  '2020'],
-         ['Änderungen _Winterthur_2017', 'Änderungen_Winterthur_2020']),
-
-    
-    # 'Änderungen Winterthur 2018', 'Änderungen Winterthur 2019' do not have AdmNo, i.e. case_id. Excluded here
 }
 
 
