038f394c086d963f126d5990ebbadeef8b071d27
liruizhang <lirui.zhang@aimedic.ch>
2022-10-14 16:39:04 +0200

    [2022-40] [WIP] Automatically create an output folder if not exist

--- src/revised_case_normalization/py/temp_file.py
+++ src/revised_case_normalization/py/temp_file.py
@@ -1,45 +1,65 @@
         
 
-    'Hirslanden Zurich': FileInfo(
+    'Hirslanden Zurich 2016': FileInfo(
          os.path.join(ROOT_DIR, 'raw_data/HI-Zurich.xlsx'),
          'Hirslanden Zurich',
          '2016',
-         ['Änderungen_Hirslanden 2016']),
+         ['Änderungen_Hirslanden 2016'])
 
-    'Hirslanden Zurich': FileInfo(
+    'Hirslanden Zurich 2017': FileInfo(
         os.path.join(ROOT_DIR, 'raw_data/HI-Zurich.xlsx'),
         'Hirslanden Zurich',
         '2017',
         ['Änderungen_Hirslanden_2017']),
 
-    'HI-Zurich.xlsx': FileInfo(
+    'Hirslanden Zurich 2018': FileInfo(
         os.path.join(ROOT_DIR, 'raw_data/HI-Zurich.xlsx'),
         'Hirslanden Zurich',
          '2018',
-        ['Änderungen_Hirslanden_2018']),
+        ['Änderungen_Hirslanden_2018'])
 
-    'HI-Zurich.xlsx': FileInfo(
+    'Hirslanden Zurich mix 2019': FileInfo(
         os.path.join(ROOT_DIR, 'raw_data/HI-Zurich.xlsx'),
-        'Hirslanden Zurich', 'Hirslanden Zurich', 'Hirslanden Zurich', 'Hirslanden Zurich'],
+        'Hirslanden Zurich',
          '2019',
-        ['Änderungen_Hirslanden 2016', 'Änderungen_Hirslanden_2017', 'Änderungen_Hirslanden_2018',
-         'Änderungen_MIX_Hirslanden_2019']),
-
-    'HI-Zurich.xlsx': FileInfo(
-        os.path.join(ROOT_DIR, 'raw_data/HI-Zurich.xlsx'),
-        ['Hirslanden Zurich', 'Hirslanden Zurich', 'Hirslanden Zurich', 'Hirslanden Zurich'],
-        ['2016', '2017', '2018', '2019'],
-        ['Änderungen_Hirslanden 2016', 'Änderungen_Hirslanden_2017', 'Änderungen_Hirslanden_2018',
-         'Änderungen_MIX_Hirslanden_2019']),
+        ['Änderungen_MIX_Hirslanden_2019']),
 
 
-
-    'HI_Aarau_Birshof_ST. Anna.xlsx': FileInfo(
+    'Hirslanden St. Anna 2017': FileInfo(
         os.path.join(ROOT_DIR, 'raw_data/HI_Aarau_Birshof_ST. Anna.xlsx'),
-        ['Hirslanden St. Anna', 'Hirslanden Aarau', 'Hirslanden Birshof', 'Hirslanden Birshof', 'Hirslanden Aarau', 'Hirslanden St. Anna'],
-        ['2017', '2017', '2017', '2018', '2018', '2018'],
-        ['Änderungen_ST. Anna_2017', 'Aarau_2017', 'Birshof_2017', 'Birshof_2018', 'Aarau 2018',
-         'Änderungen_ST. Anna_2018']),
+        'Hirslanden St. Anna',
+        '2017',
+        ['Änderungen_ST. Anna_2017'])
+
+'Hirslanden Aarau 2017': FileInfo(
+        os.path.join(ROOT_DIR, 'raw_data/HI_Aarau_Birshof_ST. Anna.xlsx'),
+         'Hirslanden Aarau',
+        '2017',
+        ['Aarau_2017'])
+'Hirslanden Birshof 2017': FileInfo(
+        os.path.join(ROOT_DIR, 'raw_data/HI_Aarau_Birshof_ST. Anna.xlsx'),
+        'Hirslanden Birshof',
+        '2017',
+        ['Birshof_2017'])
+
+'Hirslanden Birshof 2018': FileInfo(
+        os.path.join(ROOT_DIR, 'raw_data/HI_Aarau_Birshof_ST. Anna.xlsx'),
+        'Hirslanden Birshof',
+        '2018',
+        ['Birshof_2018'])
+
+
+
+'Hirslanden Aarau 2018': FileInfo(
+        os.path.join(ROOT_DIR, 'raw_data/HI_Aarau_Birshof_ST. Anna.xlsx'),
+        'Hirslanden Aarau',
+        '2018',
+        ['Aarau 2018'])
+'Hirslanden St. Anna 2018': FileInfo(
+        os.path.join(ROOT_DIR, 'raw_data/HI_Aarau_Birshof_ST. Anna.xlsx'),
+        'Hirslanden St. Anna',
+        '2018',
+        ['Änderungen_ST. Anna_2018'])
 
 
 
