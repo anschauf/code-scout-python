67d5abf9131ba55653ed6d2cfb8e89c21d32bacf
liruizhang <lirui.zhang@aimedic.ch>
2022-10-18 15:15:23 +0200

    [2022-40] [WIP] still need to work on LINDE 2018

--- src/revised_case_normalization/py/temp_file.py
+++ src/revised_case_normalization/py/temp_file.py
@@ -3,48 +3,19 @@
 
     
 
-    'Hirslanden Zurich mix 2019': FileInfo(
-        os.path.join(ROOT_DIR, 'raw_data/HI-Zurich.xlsx'),
-        'Hirslanden Zurich',
-         '2019',
-        ['Änderungen_MIX_Hirslanden_2019']),
-
-
-    'Hirslanden St. Anna 2017': FileInfo(
-        os.path.join(ROOT_DIR, 'raw_data/HI_Aarau_Birshof_ST. Anna.xlsx'),
-        'Hirslanden St. Anna',
-        '2017',
-        ['Änderungen_ST. Anna_2017'])
-
-'Hirslanden Aarau 2017': FileInfo(
-        os.path.join(ROOT_DIR, 'raw_data/HI_Aarau_Birshof_ST. Anna.xlsx'),
-         'Hirslanden Aarau',
-        '2017',
-        ['Aarau_2017'])
-'Hirslanden Birshof 2017': FileInfo(
-        os.path.join(ROOT_DIR, 'raw_data/HI_Aarau_Birshof_ST. Anna.xlsx'),
-        'Hirslanden Birshof',
-        '2017',
-        ['Birshof_2017'])
-
-'Hirslanden Birshof 2018': FileInfo(
-        os.path.join(ROOT_DIR, 'raw_data/HI_Aarau_Birshof_ST. Anna.xlsx'),
-        'Hirslanden Birshof',
-        '2018',
-        ['Birshof_2018'])
 
 
 
-'Hirslanden Aarau 2018': FileInfo(
-        os.path.join(ROOT_DIR, 'raw_data/HI_Aarau_Birshof_ST. Anna.xlsx'),
-        'Hirslanden Aarau',
-        '2018',
-        ['Aarau 2018'])
-'Hirslanden St. Anna 2018': FileInfo(
-        os.path.join(ROOT_DIR, 'raw_data/HI_Aarau_Birshof_ST. Anna.xlsx'),
-        'Hirslanden St. Anna',
-        '2018',
-        ['Änderungen_ST. Anna_2018'])
+
+
+
+
+
+
+
+
+
+
 
 
 
