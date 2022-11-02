c420931f2c8d0aa965b3df2377c18e3c8c3a7d98
Andreas Schaufelbuehl <andreas.schaufelbuehl@aimedic.ch>
2022-11-02 16:16:39 +0100

    [2022-43] WIP: add demo unit test

--- test/DemoTest.py
+++ test/DemoTest.py
@@ -3,9 +3,9 @@ import unittest
 from src.apps.code_ranking_tiers import calculate_code_ranking_performance
 
 
-class MyTestCase(unittest.TestCase):
-    def test_calculate_performance(self):
-        calculate_code_ranking_performance()
+class DemoTest(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(2+2, 4)
 
 
 if __name__ == '__main__':
