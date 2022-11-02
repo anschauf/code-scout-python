8a6a3cc7793f357053bb2ef34109703d5253ac54
liruizhang <lirui.zhang@aimedic.ch>
2022-11-02 06:54:46 +0100

    [2022-43] Refactoring Revision class

--- src/models/Revision.py
+++ src/models/Revision.py
@@ -1,8 +1,15 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, CHAR, Date, Float, SmallInteger, VARCHAR, FLOAT
-from sqlalchemy import MetaData, Table
+from sqlalchemy import MetaData
 from sqlalchemy.orm import declarative_base
 
 
+metadata_obj = MetaData(schema="some_schema")
+
+Base = declarative_base(metadata=metadata_obj)
+
+class MyClass(Base):
+    # will use "some_schema" by default
+    __tablename__ = "sometable"
 
 metadata_obj = MetaData(schema="coding_revision")
 Base = declarative_base()
