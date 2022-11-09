5e722ce8322b3288ace7edd648d55a8514204f72
Andreas Schaufelbuehl <andreas.schaufelbuehl@aimedic.ch>
2022-11-09 10:01:11 +0100

    [2022-44] Extract hardcoded column names into constants

--- src/service/aimedic_grouper.py
+++ src/service/aimedic_grouper.py
@@ -11,6 +11,26 @@ JAR_FILE_PATH = "/home/jovyan/work/resources/jars/aimedic-grouper-assembly.jar"
 SEPARATOR_CHAR = "#"
 DELIMITER_CHAR = ";"
 
+# Dataframme column names
+col_aimedic_id = 'aimedicId'
+col_diagnoses = 'diagnoses'
+col_procedures = 'procedures'
+col_grouper_result = 'grouperResult'
+col_drg_cost_weight = 'drgCostWeight'
+col_effective_cost_weight = 'effectiveCostWeight'
+col_revision_date = 'revisionDate'
+col_status = 'status'
+col_used = 'used'
+col_is_used = 'isUsed'
+col_is_primary = 'isPrimary'
+col_date_valid = 'dateValid'
+col_side_valid = 'sideValid'
+
+# Java Grouper constants
+arg_filter_valid = 'filterValid' # grouper argument, which tells the grouper it shall filter out invalid diagnoses and procedures from the case to group.
+class_path_group_many = 'ch.aimedic.grouper.BatchGroupMany'
+
+
 @beartype
 def group_batch_group_cases(batch_group_cases: list[str]) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """
@@ -37,44 +57,44 @@ def group_batch_group_cases(batch_group_cases: list[str]) -> tuple[pd.DataFrame,
         'java',
         '-cp',
         JAR_FILE_PATH,
-        'ch.aimedic.grouper.BatchGroupMany',
+        class_path_group_many,
         cases_string,
         SEPARATOR_CHAR,
         DELIMITER_CHAR,
-        'filterValid']).decode('utf-8')
+        arg_filter_valid]).decode('utf-8')
 
     # Deserialize the output into a DataFrame
     grouped_cases_dicts = json.loads(grouped_cases_json)
     complete_df = pd.DataFrame.from_dict(grouped_cases_dicts)
 
     # Prepare DataFrame for the revision table
-    revision_df = complete_df.drop(['diagnoses', 'procedures', 'grouperResult'], axis=1)
-    revision_df.rename(columns={'aimedicId': AIMEDIC_ID_COL,
-                                'drgCostWeight': DRG_COST_WEIGHT_COL,
-                                'effectiveCostWeight': EFFECTIVE_COST_WEIGHT_COL,
-                                'revisionDate': REVISION_DATE_COL
+    revision_df = complete_df.drop([col_diagnoses, col_procedures, col_grouper_result], axis=1)
+    revision_df.rename(columns={col_aimedic_id: AIMEDIC_ID_COL,
+                                col_drg_cost_weight: DRG_COST_WEIGHT_COL,
+                                col_effective_cost_weight: EFFECTIVE_COST_WEIGHT_COL,
+                                col_revision_date: REVISION_DATE_COL
                                 }, inplace=True)
 
     revision_df[REVISION_DATE_COL] = pd.to_datetime(revision_df[REVISION_DATE_COL], unit='ms')
 
     # Prepare DataFrame for the diagnoses table
-    diagnoses_df = pd.json_normalize(grouped_cases_dicts, record_path=['diagnoses'], meta=['aimedicId']) \
-        .drop(['status'], axis=1)
+    diagnoses_df = pd.json_normalize(grouped_cases_dicts, record_path=[col_diagnoses], meta=[col_aimedic_id]) \
+        .drop([col_status], axis=1)
 
-    diagnoses_df.rename(columns={'aimedicId': AIMEDIC_ID_COL,
-                                 'used': IS_GROUPER_RELEVANT_COL,
-                                 'isPrimary': IS_PRIMARY_COL
+    diagnoses_df.rename(columns={col_aimedic_id: AIMEDIC_ID_COL,
+                                 col_used: IS_GROUPER_RELEVANT_COL,
+                                 col_is_primary: IS_PRIMARY_COL
                                  }, inplace=True)
 
     diagnoses_df = diagnoses_df.reindex(columns=[AIMEDIC_ID_COL, CODE_COL, CCL_COL, IS_PRIMARY_COL, IS_GROUPER_RELEVANT_COL])
 
     # Prepare Dataframe for the procedure table
-    procedures_df = pd.json_normalize(grouped_cases_dicts, record_path=['procedures'], meta=['aimedicId']) \
-        .drop(['dateValid', 'sideValid', ], axis=1)
+    procedures_df = pd.json_normalize(grouped_cases_dicts, record_path=[col_procedures], meta=[col_aimedic_id]) \
+        .drop([col_date_valid, col_side_valid, ], axis=1)
 
-    procedures_df.rename(columns={'aimedicId': AIMEDIC_ID_COL,
-                                  'isUsed': IS_GROUPER_RELEVANT_COL,
-                                  'isPrimary': IS_PRIMARY_COL
+    procedures_df.rename(columns={col_aimedic_id: AIMEDIC_ID_COL,
+                                  col_is_used: IS_GROUPER_RELEVANT_COL,
+                                  col_is_primary: IS_PRIMARY_COL
                                   }, inplace=True)
     procedures_df[PROCEDURE_DATE_COL] = pd.to_datetime(procedures_df[PROCEDURE_DATE_COL]).dt.date
     procedures_df[PROCEDURE_SIDE_COL] = procedures_df[PROCEDURE_SIDE_COL].str.replace('\x00', '')  # replace empty byte string with an empty string
