5a4d07e83456c2b8ee88d948847ce17b9a9dea19
liruizhang <lirui.zhang@aimedic.ch>
2022-12-01 17:40:46 +0100

    [2022-46] [WIP] Refactor Unittest for grouper

--- test/service/test_aimedic_grouper.py
+++ test/service/test_aimedic_grouper.py
@@ -10,9 +10,9 @@ from src.revised_case_normalization.notebook_functions.global_configs import GRO
 class AimedicGrouperTest(unittest.TestCase):
     def test_grouper_jar_is_callable(self):
         revised_cases_df = pd.DataFrame([['HLAA|BUR#71275707|FileDate:20180227#1|Pat:07C27DA4B851FD42|Case:0041282182|in2017-out2017|67Y|W|PD:I313|SD:E788,I1090,I318,J91|SRG:009910,340999,3491,371211',
-                                          '0041282182', 'I313', ['J91', 'I318', 'E788', 'I1090', 'J9580'], '371211:L:20170216', ['340999:L:20170216', '009910::20170216', '3491:L:20170219'],
-                                          'W', 67, 0, 0, 7, 0, '01', '2017-02-16', 0, 00, '2017-02-23']],
-                                        columns=['aimedic_id', 'case_id', 'primary_diagnosis', 'secondary_diagnoses', 'primary_procedure',
+                      '0041282182', 'I313', ['J91', 'I318', 'E788', 'I1090', 'J9580'], '371211:L:20170216', ['340999:L:20170216', '009910::20170216', '3491:L:20170219'],
+                      'W', 67, 0, 0, 7, 0, '01', '2017-02-16', 0, 00, '2017-02-23']],
+                      columns=['aimedic_id', 'case_id', 'primary_diagnosis', 'secondary_diagnoses', 'primary_procedure',
                      'secondary_procedures', 'gender', 'age_years', 'age_days', 'gestation_age', 'duration_of_stay',
                      'ventilation_hours', 'grouper_admission_type', 'admission_date', 'admission_weight', 'grouper_discharge_type', 'discharge_date'])
         formatted = format_for_grouper(revised_cases_df)
