83861bb9ece96108e1277616c01f3fd1a4930a34
michael_kunz <michael.kunz@aimedic.ch>
2022-12-02 18:40:40 +0100

    [2022-46] [WIP] Added Columns for feature engineering

--- src/apps/feature_engineering.py
+++ src/apps/feature_engineering.py
@@ -4,7 +4,8 @@ import numpy as np
 import pandas as pd
 from loguru import logger
 
-from data_model.feature_engineering import HAS_VENTILATION_HOURS_COL
+from data_model.feature_engineering import HAS_VENTILATION_HOURS_COL, AGE_BINNED_COL, VENTILATION_HOURS_BINNED_COL, \
+    VENTILATION_HOURS_ADRG_NO_A_COL, EMERGENCY_COL, HAS_HOURS_IN_ICU_COL, HAS_NEMS_POINTS_COL
 from revised_case_normalization.notebook_functions.global_configs import VENTILATION_HOURS_COL
 from src.apps.feature_engineering.ccl_sensitivity import calculate_delta_pccl
 from src.apps.feature_engineering.utils import create_feature_engineering_table, store_features_in_db, validate_app_args
@@ -20,11 +21,36 @@ def create_all_features(*, chunksize: int, n_rows: Optional[int] = None):
 
     with Database() as db:
         for cases in read_cases_in_chunks(db.session, n_rows=n_rows, chunksize=chunksize):
+
             # --- Engineer features ---
+
+            # Calculating delta pccl
             cases = calculate_delta_pccl(cases, delta_value_for_max=np.nan)
             cases[HAS_VENTILATION_HOURS_COL] = cases[VENTILATION_HOURS_COL] > 0
 
+            # 'has_ventilation_hours' (boolean) * aDRG not starting with "A"
 
+            cases['adrg_no_A'] = cases['adrg'].str[0]
+            cases['adrg_no_A'] = cases['adrg_no_A'] != "A"
+            cases[VENTILATION_HOURS_ADRG_NO_A_COL] = cases['adrg_no_A'] * cases[HAS_VENTILATION_HOURS_COL]
+
+            # TODO create ventilation hours bins
+
+            # cases[VENTILATION_HOURS_COL] = cases[VENTILATION_HOURS_COL].fillna(0)
+            # cases[VENTILATION_HOURS_BINNED_COL] = pd.qcut(cases[VENTILATION_HOURS_COL], 10, duplicates='drop')
+
+            # Emergency Boolean
+            cases[EMERGENCY_COL] = cases['admission_type'] == "1"
+
+            # Hours in ICU Boolean
+            cases[HAS_HOURS_IN_ICU_COL] = cases['hours_in_icu'] > 0
+
+            # create age bins
+            age_bins = [0, 1, 2, 5, 15, 30, 40, 50, 59, 70, 80, 90, 100, 120]
+            cases[AGE_BINNED_COL] = pd.cut(cases['age_years'], age_bins)
+
+            # Nems Boolean
+            cases[HAS_NEMS_POINTS_COL] = cases['nems_total'] > 0
 
             # --- Store the data in memory ---
             if all_features is None:
@@ -41,6 +67,6 @@ def create_all_features(*, chunksize: int, n_rows: Optional[int] = None):
 
 if __name__ == '__main__':
     create_all_features(
-        chunksize=5,
-        n_rows=10
+        chunksize=1000,
+        n_rows=1000
     )
--- src/data_model/feature_engineering.py
+++ src/data_model/feature_engineering.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Float, ForeignKey, String, Boolean, CHAR
+from sqlalchemy import Column, Integer, Float, ForeignKey, String, Boolean
 from sqlalchemy import MetaData
 from sqlalchemy.orm import declarative_base
 
@@ -8,6 +8,12 @@ from src.data_model.sociodemographics import Sociodemographics
 # --- Define the feature columns ---
 DELTA_CCL_TO_NEXT_PCCL_COL = 'delta_ccl_to_next_pccl'
 HAS_VENTILATION_HOURS_COL = 'has_ventilation_hours'
+VENTILATION_HOURS_ADRG_NO_A_COL = 'ventilation_hours_adrg_no_A'
+VENTILATION_HOURS_BINNED_COL = 'ventilation_hours_binned'
+AGE_BINNED_COL = 'age_binned'
+EMERGENCY_COL = 'emergency'
+HAS_HOURS_IN_ICU_COL = 'has_hours_in_icu'
+HAS_NEMS_POINTS_COL = 'has_nems_points'
 
 # ----------------------------------
 
@@ -26,3 +32,10 @@ class FeatureEngineering(Base):
     # --- Features ---
     delta_ccl_to_next_pccl = Column(DELTA_CCL_TO_NEXT_PCCL_COL, Float)
     has_ventilation_hours = Column(HAS_VENTILATION_HOURS_COL, Boolean)
+    ventilation_hours_binned = Column(VENTILATION_HOURS_BINNED_COL, String)
+    ventilation_hours_adrg_no_A = Column(VENTILATION_HOURS_ADRG_NO_A_COL, Boolean)
+    age_binned = Column(AGE_BINNED_COL, String)
+    emergency = Column(EMERGENCY_COL, Boolean)
+    has_hours_in_icu = Column(HAS_HOURS_IN_ICU_COL, Boolean)
+    has_nems_points = Column(HAS_NEMS_POINTS_COL, Boolean)
+
