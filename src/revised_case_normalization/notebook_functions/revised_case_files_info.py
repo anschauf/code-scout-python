1aae2e0aee8eb5a6a86ef37f3ad6ccddae5fbc2d
liruizhang <lirui.zhang@aimedic.ch>
2022-12-02 20:55:02 +0100

    [2022-46] [WIP] Added all files

--- src/revised_case_normalization/notebook_functions/revised_case_files_info.py
+++ src/revised_case_normalization/notebook_functions/revised_case_files_info.py
@@ -48,23 +48,58 @@ REVISED_CASE_FILES = [
     #
     # FileInfo(os.path.join(DIR_INPUT_FILE, 'raw_data/HI_Aarau_Birshof_ST. Anna.xlsx'),
     #                      'Hirslanden Klinik St. Anna in Luzern', '2018', 'Änderungen_ST. Anna_2018'),
+    #
+    # # not work yet
+    FileInfo(os.path.join(DIR_INPUT_FILE, 'raw_data/HI-Zurich.xlsx'),
+                        'Hirslanden Klinik Zurich', '2018', 'Änderungen_Hirslanden_2018'),
 
-    # not work yet
-    # FileInfo(os.path.join(DIR_INPUT_FILE, 'raw_data/HI-Zurich.xlsx'),
-    #                     'Hirslanden Klinik Zurich', '2018', 'Änderungen_Hirslanden_2018'),
-
-    # not work yet
-    # FileInfo(os.path.join(DIR_INPUT_FILE, 'raw_data/HI-Zurich.xlsx'),
-    #                    'Hirslanden Klinik Zurich', '2016', 'Änderungen_Hirslanden 2016'),
-
+    # # not work yet
+    FileInfo(os.path.join(DIR_INPUT_FILE, 'raw_data/HI-Zurich.xlsx'),
+                       'Hirslanden Klinik Zurich', '2016', 'Änderungen_Hirslanden 2016'),
+    #
     # FileInfo(os.path.join(DIR_INPUT_FILE, 'raw_data/HI-Zurich.xlsx'),
     #                     'Hirslanden Klinik Zurich', '2017', 'Änderungen_Hirslanden_2017'),
-
-    FileInfo(os.path.join(DIR_INPUT_FILE, 'raw_data/HI-Zurich.xlsx'),
-                        'Hirslanden Klinik Zurich', '2019', 'Änderungen_MIX_Hirslanden_2019')
-
-
-
+    #
+    # FileInfo(os.path.join(DIR_INPUT_FILE, 'raw_data/HI-Zurich.xlsx'),
+    #                    'Hirslanden Klinik Zurich', '2019', 'Änderungen_MIX_Hirslanden_2019'),
+    #
+    # FileInfo(os.path.join(DIR_INPUT_FILE, 'raw_data/Linth_Toggenburg_SRRWS_2019.xlsx'),
+    #          'KSSG Spital Linth', '2019', 'Änderungen_Spital_Linth_2019'),
+    #
+    # FileInfo(os.path.join(DIR_INPUT_FILE, 'raw_data/Linth_Toggenburg_SRRWS_2019.xlsx'),
+    #          'KSSG Spitalregion Rheintal, Werdenberg, Sarganserland', '2019', 'Änderungen SRRWS_2019'),
+    #
+    # FileInfo(os.path.join(DIR_INPUT_FILE, 'raw_data/KSSG_2021.xlsx'),
+    #          'KSSG St. Gallen', '2021', 'Änderungen_KSSG_2021'),
+    #
+    # FileInfo(os.path.join(DIR_INPUT_FILE, 'raw_data/Linth_Toggenburg_SRRWS_2019.xlsx'),
+    #          'KSSG Spitalregion Fürstenland Toggenburg', '2019', 'Änderungen_Toggenburg_2019'),
+    #
+    # FileInfo(os.path.join(DIR_INPUT_FILE, 'raw_data/USZ_2018-2019_20200730.xlsx'),
+    #          'Universitätsspital Zürich', '2018', 'Rückmeldungen_USZ_2018'),
+    #
+    # FileInfo(os.path.join(DIR_INPUT_FILE, 'raw_data/USZ_2018-2019_20200730.xlsx'),
+    #          'Universitätsspital Zürich', '2020', 'Rückmeldung_USZ_2019_30.04.2020'),
+    #
+    # FileInfo(os.path.join(DIR_INPUT_FILE, 'raw_data/USZ_2018-2019_20200730.xlsx'),
+    #          'Universitätsspital Zürich', '2019', 'Gesamtauffällige_USZ_2019'),
+    #
+    # FileInfo(
+    #     os.path.join(DIR_INPUT_FILE, 'raw_data/Winterthur.xlsx'),
+    #     'Kantonsspital Winterthur', '2017', 'Änderungen _Winterthur_2017'),
+    #
+    #
+    # FileInfo(os.path.join(DIR_INPUT_FILE, 'raw_data/Winterthur.xlsx'),
+    #     'Kantonsspital Winterthur', '2018', 'Änderungen Winterthur 2018'),
+    #
+    #
+    # FileInfo(os.path.join(DIR_INPUT_FILE, 'raw_data/Winterthur.xlsx'),
+    #     'Kantonsspital Winterthur', '2019', 'Änderungen Winterthur 2019'),
+    #
+    # FileInfo(os.path.join(DIR_INPUT_FILE, 'raw_data/Winterthur.xlsx'),
+    #     'Kantonsspital Winterthur', '2020', 'Änderungen_Winterthur_2020')
+    #
+    #
 
 ]
 
