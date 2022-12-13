7274269569ff7daf7e4b385b905ff66060bdccf3
Andreas Kopf <andreas.kopf@aimedic.ch>
2022-12-13 16:35:24 +0100

    [2022-49] Analyse results from code-enrichment.

---
+++ test/sandbox_code_enrichment/plots_ksw2019.py
@@ -0,0 +1,83 @@
+from os.path import join
+
+import awswrangler as wr
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+from src.utils.general_utils import save_figure_to_pdf_on_s3
+
+THRESHOLD_ENRICHMENT = 0.05
+bucket = 'code-scout'
+s3_prefix = 's3://'
+filename = 'code_enrichment/example_ksw2019'
+dir_data = join(f'{s3_prefix}{bucket}', filename)
+dir_output = dir_data
+
+# load ground truth data from revised cases
+ground_truth = wr.s3.read_csv(join(dir_data, 'CodeScout_GroundTruthforPerformanceMeasuring.csv'))
+ground_truth = ground_truth[(ground_truth['Hospital'] == 'Kantonsspital Winterthur') & (ground_truth['Year'] == 2019)]
+ground_truth_diags_added = np.unique(np.concatenate([x.split('|') for x in ground_truth['ICD_added'].values if isinstance(x, str)]))
+ground_truth_chops_added = np.unique(np.concatenate([x.split('|') for x in ground_truth['CHOP_added'].values if isinstance(x, str)]))
+ground_truth_chops_added = np.asarray([x.split(':')[0].replace('.', '') for x in ground_truth_chops_added])
+
+# load enrichment data
+enrichment_diags = wr.s3.read_csv(join(dir_data, 't-test_diagnoses.csv'))
+enrichment_chops = wr.s3.read_csv(join(dir_data, 't-test_chops.csv'))
+
+# plot barplot diagnoses
+count_underrepresented_diags = 0
+count_overrepresented_diags = 0
+count_not_enriched_diags = 0
+for diag in ground_truth_diags_added:
+    ind_diag = np.where(enrichment_diags['diag'].values == diag)[0]
+    if len(ind_diag) == 1:
+        if enrichment_diags['pval_adj_fdr'].values[ind_diag] < THRESHOLD_ENRICHMENT:
+            if enrichment_diags['mean_all'].values[ind_diag] < enrichment_diags['mean_ksw2019'].values[ind_diag]:
+                count_overrepresented_diags += 1
+            else:
+                count_underrepresented_diags += 1
+        else:
+            count_not_enriched_diags += 1
+
+diags_enrichment_counts = [count_not_enriched_diags, count_underrepresented_diags, count_overrepresented_diags]
+data_diags = pd.DataFrame({
+    'Counts': diags_enrichment_counts,
+    'Enrichment Type': ['Not Enriched', 'Underrepresented', 'Overrepresented']
+})
+
+plt.figure()
+sns.barplot(data=data_diags, x='Enrichment Type', y='Counts')
+plt.yticks(diags_enrichment_counts, diags_enrichment_counts)
+save_figure_to_pdf_on_s3(plt, bucket=bucket, filename=join(filename, 'barplot_enrichment_diagnoses.pdf'))
+plt.close()
+
+# plot barplot chops
+count_underrepresented_chops = 0
+count_overrepresented_chops = 0
+count_not_enriched_chops = 0
+for chop in ground_truth_chops_added:
+    ind_chop = np.where(enrichment_chops['chops'].values == chop)[0]
+    if len(ind_chop) == 1:
+        if enrichment_chops['pval_adj_fdr'].values[ind_chop] < THRESHOLD_ENRICHMENT:
+            if enrichment_chops['mean_all'].values[ind_chop] < enrichment_chops['mean_ksw2019'].values[ind_chop]:
+                count_overrepresented_chops += 1
+            else:
+                count_underrepresented_chops += 1
+        else:
+            count_not_enriched_chops += 1
+
+chops_enrichment_counts = [count_not_enriched_chops, count_underrepresented_chops, count_overrepresented_chops]
+data_chops = pd.DataFrame({
+    'Counts': chops_enrichment_counts,
+    'Enrichment Type': ['Not Enriched', 'Underrepresented', 'Overrepresented']
+})
+
+plt.figure()
+sns.barplot(data=data_chops, x='Enrichment Type', y='Counts')
+plt.yticks(chops_enrichment_counts, chops_enrichment_counts)
+save_figure_to_pdf_on_s3(plt, bucket=bucket, filename=join(filename, 'barplot_enrichment_chops.pdf'))
+plt.close()
+
+print('')
