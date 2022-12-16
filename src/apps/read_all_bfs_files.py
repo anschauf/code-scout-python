cb25fcd5e2d0c215831c293885d3c0adda5e8d31
Paolo De Luna <paolo.deluna@aimedic.ch>
2022-12-16 09:37:59 +0100

    [2022-49] Completes the app `read_all_bfs_files`

--- src/apps/read_all_bfs_files.py
+++ src/apps/read_all_bfs_files.py
@@ -14,7 +14,6 @@ from src import ROOT_DIR
 from src.service.aimedic_grouper import _escape_ansi
 
 JAR_FILE_PATH = f'{ROOT_DIR}/resources/jars/aimedic-grouper-assembly.jar'
-AIMEDIC_GROUPER_CLASS = 'ch.aimedic.grouper.apps.AimedicGrouperApp'
 
 
 def read_all_bfs_files(*,
@@ -22,57 +21,80 @@ def read_all_bfs_files(*,
                        bfs_files_path: str,
                        output_dir: str
                        ):
+    """Read a list of BfS files, export all the known variables from them, and store the results into a JSON file, which
+    can be loaded into a pandas.DataFrame.
+
+    @param s3_bucket: The bucket on S3, where to find the files.
+    @param bfs_files_path: A root path where to find the files in the `s3_bucket`.
+    @param output_dir: The local output directory where to store the intermediate files, and the final concatenated file.
+
+    @note The `bfs_files_path` will be searched recursively in all its sub-folders.
+    @note All BfS files are expected to have the following pattern: `<hospital-name>_<year>.dat`
+    @note The files are processed one at a time, to avoid out-of-memory errors in the java subprocess. The files are
+        concatenated locally.
+    @note A column containing the abbreviated name of the hospital is appended to each dataset.
+    """
     root_path = f's3://{s3_bucket}/{bfs_files_path}/'
 
     # noinspection PyArgumentList
     bfs_filenames = wr.list_objects(root_path, suffix='.dat')
-    logger.info(f"Found {len(bfs_filenames)} files at '{root_path}' ...")
+    num_files = len(bfs_filenames)
+    logger.info(f"Found {num_files} files at '{root_path}'")
 
-    bfs_filenames_per_hospital = dict()
-    for bfs_filename in bfs_filenames:
-        filename = os.path.basename(bfs_filename)
-        hospital_abbreviation = filename.split('_')[0]
-
-        if hospital_abbreviation not in bfs_filenames_per_hospital:
-            bfs_filenames_per_hospital[hospital_abbreviation] = list()
-
-        bfs_filenames_per_hospital[hospital_abbreviation].append(bfs_filename)
-
-    # Clear output folder
+    # Clear the local results folder
     shutil.rmtree(output_dir, ignore_errors=True)
     Path(output_dir).mkdir(parents=True, exist_ok=True)
 
     # Append the AWS credentials to the environment variables, so that the grouper can read files from S3. We copy all
     # the variables because we need to know JAVA_HOME to be able to run java
-    env_vars = dict(os.environ)
+    env_vars = dict(os.environ)  # TODO Copy only JAVA_HOME instead of passing all the vars
     env_vars['AWS_ACCESS_KEY_ID'] = config('AWS_ACCESS_KEY_ID')
     env_vars['AWS_SECRET_ACCESS_KEY'] = config('AWS_SECRET_ACCESS_KEY')
 
-    num_hospitals = len(bfs_filenames_per_hospital)
+    # Collect the names of the temporary files, so that they can be concatenated and deleted at the end
+    output_files = list()
 
-    for idx, (hospital_abbreviation, bfs_filenames) in enumerate(bfs_filenames_per_hospital.items()):
-        logger.info(f"{idx + 1}/{num_hospitals}: Reading {len(bfs_filenames)} files for the hospital '{hospital_abbreviation}' ...")
-        # Build a comma-separated list of filenames
-        cs_bfs_filenames = ','.join(bfs_filenames)
+    for idx, bfs_filename in enumerate(bfs_filenames):
+        logger.info(f"{idx + 1}/{num_files}: Reading '{bfs_filename}' ...")
 
-        # Parse the files
+        # Parse the file
         raw_output = subprocess.check_output([
-            'java', '-cp', JAR_FILE_PATH, AIMEDIC_GROUPER_CLASS,
-            'bfs-file', "--input", cs_bfs_filenames,
-            "--all-vars"
+            'java', '-cp', JAR_FILE_PATH, 'ch.aimedic.grouper.apps.AimedicGrouperApp',
+            'bfs-file', '--input', bfs_filename,
+            '--all-vars'
         ], env=env_vars)
 
         # Read the data from the JSON format into a DataFrame
         output = _escape_ansi(raw_output.decode('UTF-8'))
         lines = output.split('\n')
         output_lines = [line for line in lines if line.startswith('{"')]
-
         df = pd.read_json('\n'.join(output_lines), orient='records', typ='frame', lines=True)
 
-        # Export the data to JSON once more, because it will preserve nested fields in some columns
-        output_path = os.path.join(output_dir, f'{hospital_abbreviation}.json')
-        logger.info(f"Exporting a pandas.DataFrame with {df.shape[0]} cases to '{output_path}' ...")
+        # Append the name of the hospital
+        filename = os.path.basename(bfs_filename)
+        hospital_abbreviation = filename.split('_')[0]
+        df['hospital'] = hospital_abbreviation
+
+        # Export the data to JSON once more (instead of, e.g., CSV), because it will preserve any nested fields
+        filename_wo_ext = os.path.splitext(filename)[0]
+        output_path = os.path.join(output_dir, f'{filename_wo_ext}.json')
+        logger.info(f"Writing {df.shape[0]} cases to '{output_path}' ...")
         df.to_json(output_path, orient='records', lines=True)
+        output_files.append(output_path)
+
+    # TODO: The output_file is hardcoded
+    output_file = os.path.join(output_dir, f'bfs_data.json')
+    logger.info(f"Concatenating all the files in '{output_file}' ...")
+
+    # Concatenate all the files into one. REF: https://stackoverflow.com/a/27077437
+    with open(output_file, 'wb') as wfd:
+        for f in output_files:
+            with open(f, 'rb') as fd:
+                shutil.copyfileobj(fd, wfd)
+
+    logger.info('Deleting all the temporary files ...')
+    for f in output_files:
+        os.remove(f)
 
     logger.success('done')
 
