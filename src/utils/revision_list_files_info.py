e16c6afcdf224d1c9ee23fd85fba5231222e6fc5
liruizhang <lirui.zhang@aimedic.ch>
2022-12-20 16:03:20 +0100

    [2022-50] Add file info from s3

--- src/utils/revision_list_files_info.py
+++ src/utils/revision_list_files_info.py
@@ -10,44 +10,48 @@ class FolderHospitalYear:
     year: int
 
 
-# DIR_REVISION_LISTS = 's3://aimedic-patient-data/revised_cases' # s3 path
-DIR_REVISION_LISTS = os.path.join(ROOT_DIR, 'src/revision_lists')
+DIR_REVISION_LISTS = 's3://aimedic-patient-data/revision_lists' # s3 path
+# DIR_REVISION_LISTS = os.path.join(ROOT_DIR, 'src/revision_lists')
+CASE_IDS_CANDIDATES = ['admno', 'fall-id']
 
 
 REVISION_LIST_INFO = [
-    FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Bern Linde 2018'),
-                     'Hirslanden Klinik Linde', 2018),
+    # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Bern_Linde_2017'),
+    #                  'Hirslanden Klinik Linde', 2017),
     #
-    # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Prüflisten SR3_ 2019'),
-    #                  'KSSG Spital Linth', 2019)
+    # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Bern_Linde_2018'),
+    #                  'Hirslanden Klinik Linde', 2018),
 
+    # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Aarau_2018'),
+    #                  'Hirslanden Klinik Aarau', 2018),
+    #
+    # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Beau_Site_2017'),
+    #                    'Hirslanden Klinik Beau-Site', 2017),
+
+    # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Beau_Site_2018'),
+    #                    'Hirslanden Klinik Beau-Site', 2018),
+    #
+    # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'KSW_2018'),
+    #                    'Kantonsspital Winterthur', 2018),
+    #
+    # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Prüflisten_KSSG_2021'),
+    #                    'KSSG St. Gallen', 2021),
+    #
+    # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Prüflisten_SR2_2019'),
+    #                    'KSSG Spital Linth', 2019),
+    #
+    # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Prüflisten_SR3_2019'),
+    #                    'KSSG Spital Linth', 2019),
+    #
+    # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Prüflisten_SR4_2019'),
+    #                    'KSSG Spitalregion Fürstenland Toggenburg', 2019),
+    #
+    # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Salem_2017'),
+    #                    'Hirslanden Salem-Spital', 2017),
+
+    FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Salem_2018'),
+                       'Hirslanden Salem-Spital', 2018),
+
+    FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'USZ_2018'),
+                       'Universitätsspital Zürich', 2018),
 ]
-# Hirslanden Clinique Bois-Cerf
-# Hirslanden Clinique Cecil
-# Hirslanden Clinique La Colline
-# Hirslanden Klinik Aarau
-# Hirslanden Klinik am Rosenberg
-# 'Hirslanden Klinik Beau-Site'.lower()
-# Hirslanden Klinik Beau-Site
-# Hirslanden Klinik Beau-Site
-# Hirslanden Kliniken Bern
-# Hirslanden Klinik Birshof
-# Hirslanden Klinik Im Park
-# 'Hirslanden Klinik Linde'.lower()
-# Hirslanden Klinik Permanence
-# Hirslanden Klinik Permanence
-# Hirslanden Klinik Permanence
-# Hirslanden Klinik St. Anna in Luzern
-# Hirslanden Klinik Stephanshorn
-# Hirslanden Klinik Zurich
-# 'Hirslanden Salem-Spital'.lower()
-# Hirslanden Salem-Spital
-# Hirslanden Salem-Spital
-# Hirslanden Salem-Spital
-# Hirslanden St. Anna in Meggen
-# ('KSSG Spital Linth'.lower())
-# 'KSSG Spitalregion Fürstenland Toggenburg'.lower()
-# "KSSG Spitalregion Rheintal, Werdenberg, Sarganserland"
-# KSSG St. Gallen
-# Kantonsspital Winterthur
-# Universitätsspital Zürich
