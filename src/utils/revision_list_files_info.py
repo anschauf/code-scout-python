30bed59e5cc5db7c62a04f0285404a658d29a7ce
liruizhang <lirui.zhang@aimedic.ch>
2022-12-20 18:57:23 +0100

    [2022-50] Updated reviewed cases in DB

--- src/utils/revision_list_files_info.py
+++ src/utils/revision_list_files_info.py
@@ -21,25 +21,29 @@ REVISION_LIST_INFO = [
     #
     # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Bern_Linde_2018'),
     #                  'Hirslanden Klinik Linde', 2018),
-
+    #
     # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Aarau_2018'),
     #                  'Hirslanden Klinik Aarau', 2018),
     #
     # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Beau_Site_2017'),
     #                    'Hirslanden Klinik Beau-Site', 2017),
-
+    #
     # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Beau_Site_2018'),
     #                    'Hirslanden Klinik Beau-Site', 2018),
-    #
+    # # no data find in bfs 2018
     # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'KSW_2018'),
     #                    'Kantonsspital Winterthur', 2018),
     #
+    # try to match 2019
+    FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'KSW_2018'),
+                       'Kantonsspital Winterthur', 2019),
+
     # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Prüflisten_KSSG_2021'),
     #                    'KSSG St. Gallen', 2021),
-    #
+    # No data in this folder
     # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Prüflisten_SR2_2019'),
-    #                    'KSSG Spital Linth', 2019),
-    #
+    #                    '', 2019),
+
     # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Prüflisten_SR3_2019'),
     #                    'KSSG Spital Linth', 2019),
     #
@@ -48,10 +52,10 @@ REVISION_LIST_INFO = [
     #
     # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Salem_2017'),
     #                    'Hirslanden Salem-Spital', 2017),
-
-    FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Salem_2018'),
-                       'Hirslanden Salem-Spital', 2018),
-
-    FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'USZ_2018'),
-                       'Universitätsspital Zürich', 2018),
+    #
+    # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'Salem_2018'),
+    #                    'Hirslanden Salem-Spital', 2018),
+    #
+    # FolderHospitalYear(os.path.join(DIR_REVISION_LISTS, 'USZ_2018'),
+    #                    'Universitätsspital Zürich', 2018),
 ]
