b528506bb01f9a29c122aa027022943fc2fe7da2
Andreas Kopf <andreas.kopf@aimedic.ch>
2023-01-09 10:50:12 +0100

    [2023-01] Simplified results summary and diagnostic plotting.

---
+++ test/sandbox_model_case_predictions/diagnostics/combine_results.py
@@ -0,0 +1,19 @@
+from os.path import join
+
+import awswrangler as wr
+import pandas as pd
+
+from src import ROOT_DIR
+from test.sandbox_model_case_predictions.utils import get_screen_summary
+
+s3_bucket: str = 'code-scout'
+prefix = f's3://{s3_bucket}'
+dir_results_s3 = 'brute_force_case_ranking_predictions/RF_5000/test2_plots'
+dir_results_local = join(ROOT_DIR, 'results', 'test2')
+summary = get_screen_summary(dir_results_local)
+
+results_s3 = wr.s3.read_csv(join(prefix, dir_results_s3, 'area_under_the_curves_top_10.csv'))
+results_local = pd.read_csv(join(dir_results_local, 'screen_summary.csv'))
+
+results_merged = pd.merge(results_s3, results_local, how='outer', on=('n_estimator', 'max_depth', 'min_sample_leaf', 'min_sample_split'))
+wr.s3.to_csv(results_merged, join(prefix, dir_results_s3, 'outer_merged_results.csv'), index=False)
---
+++ test/sandbox_model_case_predictions/diagnostics/plot_diagnostics.py
@@ -0,0 +1,69 @@
+from os.path import join
+
+import awswrangler as wr
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+from src.utils.general_utils import save_figure_to_pdf_on_s3
+
+s3_bucket: str = 'code-scout'
+prefix = f's3://{s3_bucket}'
+dir_results = 'brute_force_case_ranking_predictions/RF_5000/test2_plots'
+summary = wr.s3.read_csv(join(prefix, dir_results, 'outer_merged_results.csv'))
+
+def plot_heatmap_to_s3(dir_results, parameter_1, parameter_2):
+    for metric_name in ['precision_train', 'precision_test', 'precision_train-test',
+                        'recall_train', 'recall_test', 'recall_train-test',
+                        'f1_train', 'f1_test', 'f1_train-test',
+                        'area_normalized']:
+        combinations = summary[parameter_1].astype('string') + '_' + summary[parameter_2].astype('string')
+        if len(np.unique(combinations)) == len(combinations):
+            filename = join(dir_results, f'heatmap__{parameter_1}__VS__{parameter_2}__{metric_name}.pdf')
+        else:
+            filename = join(dir_results, f'MEAN_AGG__heatmap__{parameter_1}__VS__{parameter_2}__{metric_name}.pdf')
+
+        matrix = pd.pivot_table(summary, values=metric_name, index=[parameter_1], columns=[parameter_2], aggfunc=np.mean)
+        plt.figure()
+        sns.heatmap(matrix)
+        plt.tight_layout()
+        save_figure_to_pdf_on_s3(plt, s3_bucket, filename)
+        plt.close()
+
+
+def plot_scatter_for_metrics(RESULTS_DIR, parameter, jitter=True, jitter_scale=1):
+    for metric_name in ['precision_train', 'precision_test', 'precision_train-test',
+                        'recall_train', 'recall_test', 'recall_train-test',
+                        'f1_train', 'f1_test', 'f1_train-test',
+                        'area_normalized']:
+        plt.figure()
+        if jitter:
+            x = summary[parameter].values + np.random.normal(loc=0, scale=jitter_scale, size=summary[parameter].values.shape)
+        else:
+            x = summary[parameter].values
+        plt.scatter(x, summary[metric_name].values, s=1)
+        plt.xlabel(parameter)
+        plt.xticks(summary[parameter].values, summary[parameter].values, rotation=90)
+        plt.ylabel(metric_name)
+        plt.tight_layout()
+        save_figure_to_pdf_on_s3(plt, s3_bucket, join(RESULTS_DIR, f'scatter_{parameter}_{metric_name}.pdf'))
+        plt.close()
+
+dir_results_plots = join(dir_results, 'diagnostics')
+
+plot_heatmap_to_s3(dir_results_plots, 'n_estimator', 'max_depth')
+plot_heatmap_to_s3(dir_results_plots, 'n_estimator', 'min_sample_leaf')
+plot_heatmap_to_s3(dir_results_plots, 'n_estimator', 'min_sample_split')
+plot_heatmap_to_s3(dir_results_plots, 'min_sample_leaf', 'max_depth')
+plot_heatmap_to_s3(dir_results_plots, 'min_sample_split', 'max_depth')
+plot_heatmap_to_s3(dir_results_plots, 'min_sample_split', 'min_sample_leaf')
+
+plot_scatter_for_metrics(dir_results_plots, 'n_estimator')
+plot_scatter_for_metrics(dir_results_plots, 'max_depth')
+plot_scatter_for_metrics(dir_results_plots, 'min_sample_leaf')
+plot_scatter_for_metrics(dir_results_plots, 'min_sample_split')
+
+
+
+
