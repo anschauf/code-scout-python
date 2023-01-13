20a03c9d465b93d01656c2567bd0369f734edb16
liruizhang <lirui.zhang@aimedic.ch>
2023-01-13 16:09:46 +0100

    [2023-01] SVM using pca reduced features

--- test/sandbox_model_case_predictions/experiments/svm_leave_one_out_hyperparameter_tuning.py
+++ test/sandbox_model_case_predictions/experiments/svm_leave_one_out_hyperparameter_tuning.py
@@ -17,7 +17,7 @@ from test.sandbox_model_case_predictions.data_handler import load_data
 from test.sandbox_model_case_predictions.utils import create_predictions_output_performance_app, \
     get_list_of_all_predictors, get_revised_case_ids, RANDOM_SEED, prepare_train_eval_test_split
 
-VARIANCE_PERCENT_PCA = 0.95
+VARIANCE_PERCENT_PCA = 0.99
 
 # discarded features when using all features
 DISCARDED_FEATURES = (
@@ -111,13 +111,6 @@ def hyper_tuning_svm_pca_only_reviewed_cases():
                 cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=RANDOM_SEED)
 
                 # https://scikit-learn.org/stable/modules/model_evaluation.html#scoring-parameter
-                scoring = {
-                    'AUROC': 'roc_auc',
-                    'AUPRC': 'average_precision',
-                    'precision': 'precision',
-                    'recall': 'recall',
-                    'F1': 'f1',
-                }
 
                 scoring = {
                     'AUROC': 'roc_auc',
@@ -131,80 +124,80 @@ def hyper_tuning_svm_pca_only_reviewed_cases():
                                         return_train_score=True, return_estimator=True, error_score=np.nan,
                                         n_jobs=None, verbose=10)
 
-            RESULTS_DIR = join(ROOT_DIR, 'results', FOLDER_NAME,
-                               f'{FOLDER_NAME}_{VARIANCE_PERCENT_PCA * 100}%_{hyper_info}')
+                RESULTS_DIR = join(ROOT_DIR, 'results', FOLDER_NAME,
+                                   f'{FOLDER_NAME}_{VARIANCE_PERCENT_PCA * 100}%_{hyper_info}')
 
-            if not os.path.exists(RESULTS_DIR):
-                os.makedirs(RESULTS_DIR)
+                if not os.path.exists(RESULTS_DIR):
+                    os.makedirs(RESULTS_DIR)
 
-            logger.info('--- Average performance ---')
-            performance_log = list()
-            performance_log.append(f'# revised: {int(y.sum())}')
-            performance_log.append(f'# cases: {y.shape[0]}')
+                logger.info('--- Average performance ---')
+                performance_log = list()
+                performance_log.append(f'# revised: {int(y.sum())}')
+                performance_log.append(f'# cases: {y.shape[0]}')
 
-            longest_scorer_name = max(len(name) for name in scoring.keys())
-            for metric in scoring.keys():
-                train_metric = scores[f'train_{metric}']
-                test_metric = scores[f'test_{metric}']
-                pad = ' ' * (longest_scorer_name - len(metric) + 1)
+                longest_scorer_name = max(len(name) for name in scoring.keys())
+                for metric in scoring.keys():
+                    train_metric = scores[f'train_{metric}']
+                    test_metric = scores[f'test_{metric}']
+                    pad = ' ' * (longest_scorer_name - len(metric) + 1)
 
-                if metric == 'AUROC':
-                    suffix = ' (random 0.5)'
-                elif metric == 'AUPRC':
-                    random_performance = float(y.sum()) / y.shape[0]
-                    suffix = f' (random {random_performance:.6f})'
-                else:
-                    suffix = ''
+                    if metric == 'AUROC':
+                        suffix = ' (random 0.5)'
+                    elif metric == 'AUPRC':
+                        random_performance = float(y.sum()) / y.shape[0]
+                        suffix = f' (random {random_performance:.6f})'
+                    else:
+                        suffix = ''
 
-                msg = f'{metric}:{pad}train {np.nanmean(train_metric):.6f}, test {np.nanmean(test_metric):.6f}{suffix}'
-                performance_log.append(msg)
-                logger.info(msg)
+                    msg = f'{metric}:{pad}train {np.nanmean(train_metric):.6f}, test {np.nanmean(test_metric):.6f}{suffix}'
+                    performance_log.append(msg)
+                    logger.info(msg)
 
-            with open(join(RESULTS_DIR, 'performance.txt'), 'w') as f:
-                f.writelines('\n'.join(performance_log))
+                with open(join(RESULTS_DIR, 'performance.txt'), 'w') as f:
+                    f.writelines('\n'.join(performance_log))
 
-            logger.info('Storing models ...')
-            with open(join(RESULTS_DIR, 'svm_cv.pkl'), 'wb') as f:
-                pickle.dump(scores['estimator'], f, fix_imports=False)
+                logger.info('Storing models ...')
+                with open(join(RESULTS_DIR, 'svm_cv.pkl'), 'wb') as f:
+                    pickle.dump(scores['estimator'], f, fix_imports=False)
 
 
-            logger.info(
-                'Calculating and storing predictions for each combination of hospital and year, which contains revised cases ...')
-            # List the hospitals and years for which there are revised cases
-            all_hospitals_and_years = revised_cases_in_data[revised_cases_in_data['is_revised'] == 1][
-                ['hospital', 'dischargeYear']].drop_duplicates().values.tolist()
-            for info in tqdm(all_hospitals_and_years):
-                hospital_name = info[0]
-                discharge_year = info[1]
-                if hospital_name == LEAVE_ON_OUT[0] and discharge_year == LEAVE_ON_OUT[1]:
-                    hospital_data = revised_cases_in_data[(revised_cases_in_data['hospital'] == hospital_name) & (
-                            revised_cases_in_data['dischargeYear'] == discharge_year)]
+                logger.info(
+                    'Calculating and storing predictions for each combination of hospital and year, which contains revised cases ...')
+                # List the hospitals and years for which there are revised cases
+                all_hospitals_and_years = revised_cases_in_data[revised_cases_in_data['is_revised'] == 1][
+                    ['hospital', 'dischargeYear']].drop_duplicates().values.tolist()
+                for info in tqdm(all_hospitals_and_years):
+                    hospital_name = info[0]
+                    discharge_year = info[1]
+                    if hospital_name == LEAVE_ON_OUT[0] and discharge_year == LEAVE_ON_OUT[1]:
+                        hospital_data = revised_cases_in_data[(revised_cases_in_data['hospital'] == hospital_name) & (
+                                revised_cases_in_data['dischargeYear'] == discharge_year)]
 
-                    indices = hospital_data['index'].values
-                    case_ids = hospital_data['id'].values
+                        indices = hospital_data['index'].values
+                        case_ids = hospital_data['id'].values
 
-                    test_features = list()
-                    for feature_name in feature_names:
-                        feature_filename = feature_filenames[feature_name]
-                        feature_values = np.load(feature_filename, mmap_mode='r', allow_pickle=False, fix_imports=False)
-                        test_features.append(feature_values[indices, :])
-                    test_features = np.hstack(test_features)
+                        test_features = list()
+                        for feature_name in feature_names:
+                            feature_filename = feature_filenames[feature_name]
+                            feature_values = np.load(feature_filename, mmap_mode='r', allow_pickle=False, fix_imports=False)
+                            test_features.append(feature_values[indices, :])
+                        test_features = np.hstack(test_features)
 
-                    # scale test data and extract the top number of components for test set from pca
-                    data_rescaled_test = scaler.fit_transform(test_features)
-                    feature_test_from_pca = pca.transform(data_rescaled_test)
+                        # scale test data and extract the top number of components for test set from pca
+                        data_rescaled_test = scaler.fit_transform(test_features)
+                        feature_test_from_pca = pca.transform(data_rescaled_test)
 
-                    predictions = list()
-                    for model in scores['estimator']:
-                        predictions.append(model.predict_proba(feature_test_from_pca)[:, 1])
-                    predictions = np.mean(np.vstack(predictions), axis=0)
+                        predictions = list()
+                        for model in scores['estimator']:
+                            predictions.append(model.predict_proba(feature_test_from_pca)[:, 1])
+                        predictions = np.mean(np.vstack(predictions), axis=0)
 
-                    filename_output = join(RESULTS_DIR_TEST_PREDICTIONS,
-                                           f'{FOLDER_NAME}_{VARIANCE_PERCENT_PCA * 100}%_varaince_{hyper_info}.csv')
+                        filename_output = join(RESULTS_DIR_TEST_PREDICTIONS,
+                                               f'{FOLDER_NAME}_{VARIANCE_PERCENT_PCA * 100}%_varaince_{hyper_info}.csv')
 
-                    create_predictions_output_performance_app(filename=filename_output,
-                                                          case_ids=case_ids,
-                                                          predictions=predictions)
+                        create_predictions_output_performance_app(filename=filename_output,
+                                                              case_ids=case_ids,
+                                                              predictions=predictions)
     logger.success('done')
 
 if __name__ == '__main__':
