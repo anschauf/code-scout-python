46fc4a3b93b9ff16761095aaf9026e72003bdb74
liruizhang <lirui.zhang@aimedic.ch>
2023-01-13 16:09:04 +0100

    [2023-01] Add visualization for the first 2 and 3 pca components

--- test/sandbox_model_case_predictions/experiments/baseline_lr_feature_reduction_pca.py
+++ test/sandbox_model_case_predictions/experiments/baseline_lr_feature_reduction_pca.py
@@ -4,33 +4,28 @@ import sys
 import warnings
 from os.path import join
 
+import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-from loguru import logger
-from sklearn.linear_model import LogisticRegression
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import MinMaxScaler
 
-from sklearn.pipeline import Pipeline
+from loguru import logger
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_validate, StratifiedShuffleSplit
+from sklearn.preprocessing import MinMaxScaler
 from tqdm import tqdm
-from matplotlib import pyplot as plt
+
 from src import ROOT_DIR
 from test.sandbox_model_case_predictions.data_handler import load_data
 from test.sandbox_model_case_predictions.utils import create_predictions_output_performance_app, \
     get_list_of_all_predictors, get_revised_case_ids, RANDOM_SEED, prepare_train_eval_test_split
-
-# hand selected raw features
-USE_HAND_SELECTED_FEATURES = False
-# model selected processed features
-USE_MODEL_SELECTED_FEATURES = False
-NUM_MODEL_SELECTED_FEATURES = 200
+import pandas as pd
 
 VARIANCE_PERCENT_PCA = 0.99
 
 # discarded features when using all features
 DISCARDED_FEATURES = (
-    'hospital', 'month_admission', 'month_discharge', 'year_discharge', 'mdc_OHE', 'hauptkostenstelle_OHE', 'vectorized_codes')
+    'hospital', 'month_admission', 'month_discharge', 'year_discharge', 'mdc_OHE', 'hauptkostenstelle_OHE',
+    'vectorized_codes')
 
 # create folder names for output
 LEAVE_ON_OUT = ('KSW', 2020)
@@ -48,7 +43,7 @@ HYPERPARAMETER = {'C': 1.0, 'penalty': 'l2', 'solver': 'saga'}
 hyperparameter_info = '_'.join([f'{key}-{value}' for key, value in HYPERPARAMETER.items()])
 FOLDER_NAME = f'lr_baseline_use_PCA_reduced_features_{LEAVE_ON_OUT[0]}_{LEAVE_ON_OUT[1]}'
 RESULTS_DIR = join(ROOT_DIR, 'results', FOLDER_NAME,
-                       f'{FOLDER_NAME}_{VARIANCE_PERCENT_PCA*100}%_varaince_{hyperparameter_info}')
+                   f'{FOLDER_NAME}_{VARIANCE_PERCENT_PCA * 100}%_varaince_{hyperparameter_info}')
 
 if not os.path.exists(RESULTS_DIR):
     os.makedirs(RESULTS_DIR)
@@ -66,12 +61,10 @@ def feature_reduction_only_reviewed_cases():
     features_dir = join(ROOT_DIR, 'resources', 'features')
     feature_filenames, encoders = get_list_of_all_predictors(all_data, features_dir, overwrite=False)
     feature_names = sorted(list(feature_filenames.keys()))
-    if USE_HAND_SELECTED_FEATURES:
-        feature_names = [feature_name for feature_name in feature_names
-                         if any(feature_name.startswith(hand_selected_features) for hand_selected_features in HAND_SELECTED_FEATURES)]
-    else:
-        feature_names = [feature_name for feature_name in feature_names
-                         if not any(feature_name.startswith(discarded_feature) for discarded_feature in DISCARDED_FEATURES)]
+
+    feature_names = [feature_name for feature_name in feature_names
+                     if not any(
+            feature_name.startswith(discarded_feature) for discarded_feature in DISCARDED_FEATURES)]
 
     revised_cases_in_data = get_revised_case_ids(all_data, REVISED_CASE_IDS_FILENAME, overwrite=False)
     # create_performance_app_ground_truth(dir_output, revised_cases_in_data, hospital_year_for_performance_app[0], hospital_year_for_performance_app[1])
@@ -107,9 +100,47 @@ def feature_reduction_only_reviewed_cases():
     features_train = features[ind_train_test]
     y_train = y[ind_train_test]
 
-    #  preprocessing features (scaler)
+    # preprocessing features (scaler)
     scaler = MinMaxScaler()
     data_rescaled = scaler.fit_transform(features_train)
+
+    # get the first 2 priciple components for visualization
+    pca_2 = PCA(n_components=2)
+    train_components = pca_2.fit_transform(data_rescaled)
+
+    train_components_df = pd.DataFrame(train_components, columns=['component1', 'component2'])
+    train_components_df['revised'] = y_train
+    labels = ['not revised', 'revised']
+    colors = ['red', 'green']
+
+    for i, dff in train_components_df.groupby("revised"):
+        col = dff.shape[0]*[colors[i]]
+        plt.scatter(dff['component1'], dff['component2'], c=col, label= labels[i])
+
+    plt.legend()
+    # train_components_df.plot.scatter(x='component1', y='component2',c=colors)
+    plt.savefig('pca_2.png')
+
+    # get the first 3 priciple components for visualization
+    pca_3 = PCA(n_components=3)
+    train_components_3 = pca_3.fit_transform(data_rescaled)
+
+    train_components_3_df = pd.DataFrame(train_components_3, columns=['component1', 'component2', 'component3'])
+    train_components_3_df['revised'] = y_train
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = plt.axes(projection='3d')
+    colors_3 = ['red' if i==0 else 'green' for i in y_train]
+
+    for i, dff in train_components_3_df.groupby("revised"):
+        col = dff.shape[0] * [colors[i]]
+        ax.scatter3D(dff['component1'], dff['component2'],
+                     dff['component3'], c=col, label=labels[i])
+
+    plt.legend()
+    plt.savefig('pca_3.png')
+
+    # get certain amount of variance
     pca = PCA(VARIANCE_PERCENT_PCA)
 
     pca.fit(data_rescaled)
@@ -117,7 +148,7 @@ def feature_reduction_only_reviewed_cases():
     # get the top number of components
     feature_train_pca = pca.transform(data_rescaled)
 
-    logger.info(f'{n_com} of principle components contribute to {VARIANCE_PERCENT_PCA*100}% variance')
+    logger.info(f'{n_com} of principle components contribute to {VARIANCE_PERCENT_PCA * 100}% variance')
 
     with warnings.catch_warnings():
         warnings.simplefilter('ignore')
@@ -131,14 +162,6 @@ def feature_reduction_only_reviewed_cases():
         cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=RANDOM_SEED)
 
         # https://scikit-learn.org/stable/modules/model_evaluation.html#scoring-parameter
-        scoring = {
-            'AUROC': 'roc_auc',
-            'AUPRC': 'average_precision',
-            'precision': 'precision',
-            'recall': 'recall',
-            'F1': 'f1',
-        }
-
         scoring = {
             'AUROC': 'roc_auc',
             'AUPRC': 'average_precision',
@@ -181,7 +204,6 @@ def feature_reduction_only_reviewed_cases():
     with open(join(RESULTS_DIR, 'lr_baseline_cv.pkl'), 'wb') as f:
         pickle.dump(scores['estimator'], f, fix_imports=False)
 
-
     logger.info(
         'Calculating and storing predictions for each combination of hospital and year, which contains revised cases ...')
     # List the hospitals and years for which there are revised cases
@@ -221,8 +243,6 @@ def feature_reduction_only_reviewed_cases():
                                                       predictions=predictions)
 
     logger.success('done')
-
-
 
 
 if __name__ == '__main__':
