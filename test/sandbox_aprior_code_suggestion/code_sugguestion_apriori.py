5239846f258c1b36bccee79528ce15daa846b607
liruizhang <lirui.zhang@aimedic.ch>
2023-01-27 15:00:40 +0100

    [2023-01] (WIP)

--- test/sandbox_aprior_code_suggestion/code_sugguestion_apriori.py
+++ test/sandbox_aprior_code_suggestion/code_sugguestion_apriori.py
@@ -1,12 +1,20 @@
-
+import pandas as pd
 from src.service.bfs_cases_db_service import get_all_diagonosis, get_sociodemographics_for_hospital_year, \
     get_revised_case_with_codes_before_revision, get_revised_case_with_codes_after_revision
 from src.service.database import Database
 from efficient_apriori import apriori
-
+from test.sandbox_model_case_predictions.data_handler import DTYPES
+from src import ROOT_DIR
+import os
 
 hospital = "Kantonsspital Winterthur"
+file_name = 'KSW_2020.json'
 year = 2020
+# get the hospital and year
+dir_data = os.path.join(ROOT_DIR, 'resources', 'data'),
+# df = pd.read_json(os.path.join(dir_data, 'KSW_2020.json'), lines=True, dtype=DTYPES)
+
+#revised_cases_in_data = get_revised_case_ids(all_data, REVISED_CASE_IDS_FILENAME, overwrite=False)
 
 with Database() as db:
     all_codes = get_all_diagonosis(db.session)
@@ -34,20 +42,21 @@ case_sd_after_revision = example_case_after_revision['secondary_diagnoses']
 
 
 
-d  = all_codes.groupby('revision_id', group_keys=False)['code'].apply(list).reset_index()
-
+d  = all_codes.groupby(['sociodemographic_id','revision_id'], as_index=False).agg({'code': lambda x: list(x), 'is_primary': lambda x: list(x), 'ccl': lambda x: list(x)})
 case_with_case_pd = d[d['code'].apply(lambda row: case_pd_after_revision in row)]
 
-# d.to_csv("all_cases_diagnosis.csv")
+d.to_csv("all_cases_diagnosis.csv")
 
-codes = [tuple(row) for row in case_with_case_pd['code'].values.tolist()]
+# codes = [tuple(row) for row in case_with_case_pd['code'].values.tolist()]
+codes_all = [tuple(row) for row in d['code'].values.tolist()]
 
 min_support = 1.0/case_with_case_pd.shape[0]
 
+itemsets, rules = apriori(codes_all, max_length=3)
 # for max_length in [2, 3, 4, 5]:
 for max_length in [2]:
     itemset_info = f'intemsets_{max_length=}'
-    itemsets, rules = apriori(codes, max_length=max_length, min_support=min_support, min_confidence=1)
+    itemsets, rules = apriori(codes_all, max_length=max_length, min_support=min_support, min_confidence=1)
 
-
+# Print a preview of the frequent itemsets
 print('')
