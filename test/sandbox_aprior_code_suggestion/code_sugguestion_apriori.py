b02507a2d1639aabae16bdd5887535f91fd9b48e
liruizhang <lirui.zhang@aimedic.ch>
2023-01-30 13:44:06 +0100

    [2023-01] (WIP)

--- test/sandbox_aprior_code_suggestion/code_sugguestion_apriori.py
+++ test/sandbox_aprior_code_suggestion/code_sugguestion_apriori.py
@@ -6,6 +6,9 @@ from efficient_apriori import apriori
 from test.sandbox_model_case_predictions.data_handler import DTYPES
 from src import ROOT_DIR
 import os
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules
+
 
 hospital = "Kantonsspital Winterthur"
 file_name = 'KSW_2020.json'
@@ -16,47 +19,132 @@ dir_data = os.path.join(ROOT_DIR, 'resources', 'data'),
 
 #revised_cases_in_data = get_revised_case_ids(all_data, REVISED_CASE_IDS_FILENAME, overwrite=False)
 
-with Database() as db:
-    all_codes = get_all_diagonosis(db.session)
-    sociodemo = get_sociodemographics_for_hospital_year(hospital_name=hospital, year=2020, session=db.session)
-    # maybe it will be better only get the sociodemographic_ids for ksw 2020
-    sociodemo_ids = sociodemo['sociodemographic_id'].tolist()
+# with Database() as db:
+#     all_codes = get_all_diagonosis(db.session)
+#     sociodemo = get_sociodemographics_for_hospital_year(hospital_name=hospital, year=2020, session=db.session)
+#     # maybe it will be better only get the sociodemographic_ids for ksw 2020
+#     sociodemo_ids = sociodemo['sociodemographic_id'].tolist()
+#
+#     revised_cases_before_revision = get_revised_case_with_codes_before_revision(db.session)
+#     revised_cases_after_revision = get_revised_case_with_codes_after_revision(db.session)
+#     revised_cases_sociodemographic_id = revised_cases_before_revision['sociodemographic_id'].tolist()
+#     revised_cases_sociodemographic_id_ksw = [sociodemo_id for sociodemo_id in revised_cases_sociodemographic_id if sociodemo_id in sociodemo_ids]
+#
+#     revised_cases_before_revision_ksw = revised_cases_before_revision[revised_cases_before_revision['sociodemographic_id'].isin(revised_cases_sociodemographic_id_ksw)]
+#     revised_cases_after_revision_ksw = revised_cases_after_revision[revised_cases_after_revision['sociodemographic_id'].isin(revised_cases_sociodemographic_id_ksw)]
+#
+# example_case_before_revision = revised_cases_before_revision_ksw.iloc[0]
+#
+# case_sociodemo_id = example_case_before_revision['sociodemographic_id']
+# case_pd_before_revision = example_case_before_revision["pd"]
+# case_sd_before_revision = example_case_before_revision['secondary_diagnoses']
+#
+# example_case_after_revision = revised_cases_after_revision_ksw[revised_cases_after_revision['sociodemographic_id']==case_sociodemo_id]
+# case_pd_after_revision = example_case_after_revision["pd"].values[0]
+# case_sd_after_revision = example_case_after_revision['secondary_diagnoses']
+#
 
-    revised_cases_before_revision = get_revised_case_with_codes_before_revision(db.session)
-    revised_cases_after_revision = get_revised_case_with_codes_after_revision(db.session)
-    revised_cases_sociodemographic_id = revised_cases_before_revision['sociodemographic_id'].tolist()
-    revised_cases_sociodemographic_id_ksw = [sociodemo_id for sociodemo_id in revised_cases_sociodemographic_id if sociodemo_id in sociodemo_ids]
 
-    revised_cases_before_revision_ksw = revised_cases_before_revision[revised_cases_before_revision['sociodemographic_id'].isin(revised_cases_sociodemographic_id_ksw)]
-    revised_cases_after_revision_ksw = revised_cases_after_revision[revised_cases_after_revision['sociodemographic_id'].isin(revised_cases_sociodemographic_id_ksw)]
+# d  = all_codes.groupby(['sociodemographic_id','revision_id'], as_index=False).agg({'code': lambda x: list(x), 'is_primary': lambda x: list(x), 'ccl': lambda x: list(x)})
+# case_with_case_pd = d[d['code'].apply(lambda row: case_pd_after_revision in row)]
 
-example_case_before_revision = revised_cases_before_revision_ksw.iloc[0]
+# d.to_csv("all_cases_diagnosis.csv")
 
-case_sociodemo_id = example_case_before_revision['sociodemographic_id']
-case_pd_before_revision = example_case_before_revision["pd"]
-case_sd_before_revision = example_case_before_revision['secondary_diagnoses']
+d = pd.read_csv(os.path.join(ROOT_DIR, 'test/sandbox_aprior_code_suggestion/all_cases_diagnosis.csv'))
+# min_support = 1.0/case_with_case_pd.shape[0]
 
-example_case_after_revision = revised_cases_after_revision_ksw[revised_cases_after_revision['sociodemographic_id']==case_sociodemo_id]
-case_pd_after_revision = example_case_after_revision["pd"].values[0]
-case_sd_after_revision = example_case_after_revision['secondary_diagnoses']
+min_support = 0.001
+# clear the code and save as a list
+def clean_alt_list(list_):
+    list_ = list_.strip('[ ]')
+    list_ = list_.replace("'", '')
+    list_ = list_.split(', ')
+
+    return list_
+# case_need to get the suggestions
+example_ksw_2020_revised_cases_before_revision = pd.read_csv(os.path.join(ROOT_DIR, 'test/sandbox_aprior_code_suggestion/example_ksw_2020_revised_cases_before_revision.csv'))
+example_ksw_2020_revised_cases_before_revision['secondary_diagnoses'] = example_ksw_2020_revised_cases_before_revision['secondary_diagnoses'].apply(clean_alt_list)
+example_ksw_2020_revised_cases_before_revision
+
+    # n = 0
+for n in [1 ,2 , 3, 4]:
+    row_n = example_ksw_2020_revised_cases_before_revision.iloc[n]
+
+    revision_id = row_n['revision_id']
+    sociodemographic_id = row_n['sociodemographic_id']
+
+    case_pd = row_n['pd']
+    sd = row_n['secondary_diagnoses']
+    case_icds = set(sd)
+    case_icds.add(case_pd)
+    case_info = f'{revision_id=}_{sociodemographic_id=}'
+    case_icds
+
+    # filter out the data contain any codes from the case need suggestions
+    d['code_list'] = d['code'].apply(clean_alt_list)
+    d['contain_case_icds'] = d['code_list'].apply(lambda x: len(case_icds.intersection(set(x))) > 0)
+    icds_subset = d[d['contain_case_icds']==True]
+    print(icds_subset.shape)
 
 
 
-d  = all_codes.groupby(['sociodemographic_id','revision_id'], as_index=False).agg({'code': lambda x: list(x), 'is_primary': lambda x: list(x), 'ccl': lambda x: list(x)})
-case_with_case_pd = d[d['code'].apply(lambda row: case_pd_after_revision in row)]
+    # Instantiate transaction encoder and identify unique items in transactions
+    encoder = TransactionEncoder()
 
-d.to_csv("all_cases_diagnosis.csv")
+    # One-hot encode transactions
+    subset_icds_list = icds_subset['code_list'].to_list()
+    fitted = encoder.fit_transform(subset_icds_list, sparse=True)
 
-# codes = [tuple(row) for row in case_with_case_pd['code'].values.tolist()]
-codes_all = [tuple(row) for row in d['code'].values.tolist()]
+    df_icds_onehot = pd.DataFrame.sparse.from_spmatrix(fitted, columns=encoder.columns_) # seemed to work good
+    df_icds_onehot.shape
 
-min_support = 1.0/case_with_case_pd.shape[0]
+    # Compute frequent itemsets using the Apriori algorithm
+    import tqdm
 
-itemsets, rules = apriori(codes_all, max_length=3)
-# for max_length in [2, 3, 4, 5]:
-for max_length in [2]:
-    itemset_info = f'intemsets_{max_length=}'
-    itemsets, rules = apriori(codes_all, max_length=max_length, min_support=min_support, min_confidence=1)
+    frequent_itemsets = apriori(df_icds_onehot,
+                                min_support = 0.001,
+                                max_len = 4,
+                                verbose=1,
+                                low_memory=True,
+                                use_colnames=True)
 
-# Print a preview of the frequent itemsets
+
+    # Print a preview of the frequent itemsets
+    print(len(frequent_itemsets))
+
+    # define rules based on the metrics support
+
+    frequent_itemsets_rules_1 = association_rules(frequent_itemsets,
+                                metric = "support",
+                                min_threshold = 0.0015)
+    # sort the rules based on the confidence and lift
+    frequent_itemsets_rules_1 = frequent_itemsets_rules_1.sort_values(['confidence', 'lift'], ascending =[False, False])
+
+
+    # change the antecedents and consequents from frozen set to list
+    frequent_itemsets_rules_1['antecedents_ls']= frequent_itemsets_rules_1['antecedents'].apply(lambda x: list(x))
+    frequent_itemsets_rules_1['consequents_ls']= frequent_itemsets_rules_1['consequents'].apply(lambda x: list(x))
+
+    # filter rule which contains the codes from example cases
+    frequent_itemsets_rules_1['case_rule_1']= frequent_itemsets_rules_1['antecedents_ls'].apply(lambda x: len(set(x).intersection(case_icds))==1)
+    frequent_itemsets_rules_1['case_rule_2']= frequent_itemsets_rules_1['antecedents_ls'].apply(lambda x: len(set(x).intersection(case_icds))==2)
+    frequent_itemsets_rules_1['case_rule_3']= frequent_itemsets_rules_1['antecedents_ls'].apply(lambda x: len(set(x).intersection(case_icds))==3)
+
+    frequent_itemsets_rules_1['case_rule_result']= frequent_itemsets_rules_1['consequents_ls'].apply(lambda x: len(set(x).intersection(case_icds))==0)
+
+
+    # Get the suggests based on one code, two code
+    rule1 = frequent_itemsets_rules_1[(frequent_itemsets_rules_1['case_rule_1'] == True)& (frequent_itemsets_rules_1['case_rule_result'] == True)]
+    rule2 = frequent_itemsets_rules_1[(frequent_itemsets_rules_1['case_rule_2'] == True)& (frequent_itemsets_rules_1['case_rule_result'] == True)]
+    rule3 = frequent_itemsets_rules_1[(frequent_itemsets_rules_1['case_rule_3'] == True)& (frequent_itemsets_rules_1['case_rule_result'] == True)]
+
+    rule2.to_csv(f'Suggest_for_{case_info}_based_on_two_codes.csv')
+    rule1.to_csv(f'Suggest_for_{case_info}_based_on_one_code.csv')
+    rule3.to_csv(f'Suggest_for_{case_info}_based_on_three_codes.csv')
+
 print('')
+
+'revision_id=816275_sociodemographic_id=816247'
+
+
+# get the case before revision
--- test/service/test_aimedic_grouper.py
+++ test/service/test_aimedic_grouper.py
@@ -1,8 +1,10 @@
 import unittest
-
 import pandas as pd
 
 from src.service.aimedic_grouper import AIMEDIC_GROUPER
+from src.service.bfs_cases_db_service import get_sociodemographics_for_hospital_year, \
+    get_original_revision_id_for_sociodemographic_ids, get_revised_case_with_codes_before_revision, \
+    get_revised_case_with_codes_after_revision
 from src.utils.global_configs import GROUPER_FORMAT_COL
 from src.utils.group import format_for_grouper
 
@@ -11,7 +13,7 @@ from src.service.database import Database
 class AimedicGrouperTest(unittest.TestCase):
     def test_grouper_jar_is_callable(self):
         revised_cases_df = pd.DataFrame([['100',
-                      '0041282182', 'I313', ['J91', 'I318', 'E788', 'I1090', 'J9580'], '371211:L:20170216', ['340999:L:20170216', '009910::20170216', '3491:L:20170219'],
+                      '21320891027', 'R21', ['J91', 'I318', 'E788', 'I1090', 'J9580'], '371211:L:20170216', ['340999:L:20170216', '009910::20170216', '3491:L:20170219'],
                       'W', 67, 0, 0, 7, 0, '01', '2017-02-16', 0, 00, '2017-02-23', '']],
                       columns=['sociodemographic_id', 'case_id', 'primary_diagnosis', 'secondary_diagnoses', 'primary_procedure',
                      'secondary_procedures', 'gender', 'age_years', 'age_days', 'gestation_age', 'duration_of_stay',
@@ -26,6 +28,58 @@ class AimedicGrouperTest(unittest.TestCase):
         df = AIMEDIC_GROUPER.run_batch_grouper(cases=[grouper_format, grouper_format2])
         self.assertEqual(df.shape[0], 2)
 
+
+
+        sd_orginal = ['I1090' ,'I481' ,'Z921' ,'G406' ,'I8720' ,'Z867' ,'Z950']
+        sd_add = ''
+    def test_suggested_codes_grouper(self):
+        with Database() as db:
+            sociodemo_hospital_year = get_sociodemographics_for_hospital_year(
+                hospital_name='Kantonsspital Winterthur', year=2020, session=db.session)
+            sociodemographic_id = sociodemo_hospital_year['sociodemographic_id'].tolist()
+            sociodemographic_id_revision_id_df = get_original_revision_id_for_sociodemographic_ids(
+                sociodemographic_id, db.session)
+
+            # case need to be tested
+            revised_cases_before_revision = get_revised_case_with_codes_before_revision(db.session)
+            revised_cases_after_revision = get_revised_case_with_codes_after_revision(db.session)
+            revised_cases_sociodemographic_id = revised_cases_before_revision['sociodemographic_id'].tolist()
+            revised_cases_sociodemographic_id_ksw = [sociodemo_id for sociodemo_id in
+                                                     revised_cases_sociodemographic_id
+                                                     if sociodemo_id in sociodemographic_id]
+
+            revised_cases_before_revision_ksw_2020 = revised_cases_before_revision[
+                revised_cases_before_revision['sociodemographic_id'].isin(revised_cases_sociodemographic_id_ksw)]
+            revised_cases_after_revision_ksw_2020 = revised_cases_after_revision[
+                revised_cases_after_revision['sociodemographic_id'].isin(revised_cases_sociodemographic_id_ksw)]
+
+            print('')
+            # Test 5 revised cases
+            sociodemo_id_revised_5 = revised_cases_before_revision_ksw_2020['sociodemographic_id'].tolist()[:5]
+            revised_cases_before_revision_5 = revised_cases_before_revision_ksw_2020[
+                revised_cases_before_revision_ksw_2020['sociodemographic_id'].isin(sociodemo_id_revised_5)]
+            revision_id_5 = revised_cases_before_revision_5['revision_id'].tolist()
+            sociodemo_info_5 = sociodemo_hospital_year[
+                sociodemo_hospital_year['sociodemographic_id'].isin(sociodemo_id_revised_5)]
+
+            revised_cases_before_revision_5.set_index('sociodemographic_id', inplace=True)
+            sociodemo_info_5.set_index('sociodemographic_id', inplace=True)
+
+            all_info_case_5 = pd.concat(
+                [sociodemo_info_5, revised_cases_before_revision_5], axis=1)
+            all_info_case_5.reset_index(inplace=True)
+            print(all_info_case_5.columns)
+            colums_for_grouper = all_info_case_5[['sociodemographic_id', 'case_id', 'primary_diagnosis', 'secondary_diagnoses', 'primary_procedure',
+                     'secondary_procedures', 'gender', 'age_years', 'age_days', 'gestation_age', 'duration_of_stay',
+                     'ventilation_hours', 'grouper_admission_type', 'admission_date', 'admission_weight', 'grouper_discharge_type', 'discharge_date', 'medications']]
+            # colums_for_grouper = all_info_case_5[['sociodemographic_id', 'case_id']]
+            # Prepare the format for grouper
+            revision_id = 816275
+            sociodemographic_id = 816247
+            socio_demo_case = sociodemo_hospital_year[sociodemo_hospital_year['']]
+
+            print('')
+            # df = pd.DataFrame()
 
 if __name__ == '__main__':
     unittest.main()
