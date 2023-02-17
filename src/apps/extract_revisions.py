476c51b03b82a45811564f33821e6530670c018c
liruizhang <lirui.zhang@aimedic.ch>
2023-02-17 14:51:08 +0100

    [2023-07] Extract revision with information that if an added code is grouper relevant or not

--- src/apps/extract_revisions.py
+++ src/apps/extract_revisions.py
@@ -20,8 +20,10 @@ def extract_revisions():
 
         logger.info('Reading the sociodemographic data of these cases ...')
         original_cases_ids = original_cases['sociodemographic_id'].values.tolist()
-        sociodemographics = get_sociodemographics_by_sociodemographics_ids(original_cases_ids, db.session)[['sociodemographic_id', 'case_id', 'hospital_id']]
-        hospitals = get_hospitals(db.session)[['hospital_id', 'hospital_abbreviation']].rename(columns={'hospital_abbreviation': 'hospital'})
+        sociodemographics = get_sociodemographics_by_sociodemographics_ids(original_cases_ids, db.session)[
+            ['sociodemographic_id', 'case_id', 'hospital_id']]
+        hospitals = get_hospitals(db.session)[['hospital_id', 'hospital_abbreviation']].rename(
+            columns={'hospital_abbreviation': 'hospital'})
         sociodemographics = pd.merge(sociodemographics, hospitals, on='hospital_id').drop(columns=['hospital_id'])
 
         logger.info('Reading the revised cases after revision ...')
@@ -39,19 +41,21 @@ def extract_revisions():
     df = df[sorted_column_pairs]
 
     def _extract_revision(row):
-        original_diagnoses = [row['pd_original']]
-        original_diagnoses.extend(sorted(row['secondary_diagnoses_original']))
-        revised_diagnoses = [row['pd_revised']]
-        revised_diagnoses.extend(sorted(row['secondary_diagnoses_revised']))
+        original_diagnoses = row['all_diagnoses_original']
+        revised_diagnoses = row['all_diagnoses_revised']
 
-        row['original_diagnoses'] = original_diagnoses
-        row['revised_diagnoses'] = revised_diagnoses
-        row['added_diagnoses'] = tuple(set(revised_diagnoses).difference(original_diagnoses))
+        add_diagnoses = tuple(set(revised_diagnoses).difference(set(original_diagnoses)))
+        row['added_diagnoses'] = add_diagnoses
 
-        original_procedures = [row['primary_procedure_original']]
-        original_procedures.extend(row['secondary_procedures_original'])
-        revised_procedures = [row['primary_procedure_revised']]
-        revised_procedures.extend(row['secondary_procedures_revised'])
+        idx_add_diagnoses = [revised_diagnoses.index(added_diag) for added_diag in add_diagnoses]
+
+        is_grouper_relevant_diagnoses_revised = row['is_grouper_relevant_diagnoses_revised']
+        assert len(is_grouper_relevant_diagnoses_revised) == len(revised_diagnoses), 'Case has deleted diagnoses'
+        row['added_diagnoses_grouper_relevant'] = set(
+            [is_grouper_relevant_diagnoses_revised[idx] for idx in idx_add_diagnoses])
+
+        original_procedures = row['all_procedures_original']
+        revised_procedures = row['all_procedures_revised']
 
         original_procedures = [p.split(':')[0] for p in original_procedures]
         original_procedures = [p for p in original_procedures if p != '' and p != 'nan']
@@ -59,18 +63,30 @@ def extract_revisions():
         revised_procedures = [p.split(':')[0] for p in revised_procedures]
         revised_procedures = [p for p in revised_procedures if p != '' and p != 'nan']
 
-        row['original_procedures'] = sorted(original_procedures)
-        row['revised_procedures'] = sorted(revised_procedures)
-        row['added_procedures'] = tuple(set(revised_procedures).difference(original_procedures))
+        added_procedures = tuple(set(revised_procedures).difference(set(original_procedures)))
+        row['added_procedures'] = added_procedures
+        idx_add_procedures = [revised_procedures.index(added_proc) for added_proc in added_procedures]
+        is_grouper_relevant_procedure_revised = row['is_grouper_relevant_procedures_revised']
+        assert len(is_grouper_relevant_diagnoses_revised) == len(revised_diagnoses), 'Case has deleted procedures'
+        row['added_procedure_grouper_relevant'] = set(
+            [is_grouper_relevant_procedure_revised[idx] for idx in idx_add_procedures])
 
         return row
 
     logger.info('Extracting the list of added diagnosis and procedure codes ...')
     df = df.apply(_extract_revision, axis=1)
 
-    df.drop(columns=['reviewed_original', 'revised_original', 'revision_date_original', 'reviewed_revised', 'revised_revised', 'revision_date_revised', 'revision_id_original', 'revision_id_revised',
+    # save csv with all info : can be used for analysis drg relevant codes before or after revision
+    output_path_revised_all_info = os.path.join(ROOT_DIR, 'resources', 'db_data', 'revised_case_all_info.csv')
+    os.makedirs(os.path.dirname(output_path_revised_all_info), exist_ok=True)
+    logger.info(f"Storing {df.shape[0]} cases at {output_path_revised_all_info} ...")
+    df.to_csv(output_path_revised_all_info, index=False)
+
+    df.drop(columns=['reviewed_original', 'revised_original', 'revision_date_original', 'reviewed_revised',
+                     'revised_revised', 'revision_date_revised', 'revision_id_original', 'revision_id_revised',
                      'pd_original', 'secondary_diagnoses_original', 'pd_revised', 'secondary_diagnoses_revised',
-                     'primary_procedure_original', 'secondary_procedures_original', 'primary_procedure_revised', 'secondary_procedures_revised',
+                     'primary_procedure_original', 'secondary_procedures_original', 'primary_procedure_revised',
+                     'secondary_procedures_revised',
                      'adrg_original', 'adrg_revised',
                      ], inplace=True)
 
