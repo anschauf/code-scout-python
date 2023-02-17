f72a95126c28ed0e3825db600e46bd36a5cdc513
liruizhang <lirui.zhang@aimedic.ch>
2023-02-17 15:50:14 +0100

    [2023-07] Fix a small bug and analysis if the added procedure is grouper relevant or not

--- src/apps/extract_revisions.py
+++ src/apps/extract_revisions.py
@@ -1,3 +1,5 @@
+import itertools
+import itertools
 import os.path
 
 import pandas as pd
@@ -44,15 +46,18 @@ def extract_revisions():
         original_diagnoses = row['all_diagnoses_original']
         revised_diagnoses = row['all_diagnoses_revised']
 
-        add_diagnoses = tuple(set(revised_diagnoses).difference(set(original_diagnoses)))
-        row['added_diagnoses'] = add_diagnoses
-
-        idx_add_diagnoses = [revised_diagnoses.index(added_diag) for added_diag in add_diagnoses]
-
+        diff_diagnoses = tuple(set(revised_diagnoses).difference(set(original_diagnoses)))
         is_grouper_relevant_diagnoses_revised = row['is_grouper_relevant_diagnoses_revised']
-        assert len(is_grouper_relevant_diagnoses_revised) == len(revised_diagnoses), 'Case has deleted diagnoses'
-        row['added_diagnoses_grouper_relevant'] = set(
-            [is_grouper_relevant_diagnoses_revised[idx] for idx in idx_add_diagnoses])
+
+        add_diagnoses_true = [diag for diag in diff_diagnoses if diag not in original_diagnoses]
+
+        if len(diff_diagnoses) != len(add_diagnoses_true):
+            case_id = row['case_id']
+            logger.info(f'Case {case_id} has deleted diagnoses')
+        idx_add_diagnoses = [revised_diagnoses.index(added_diag) for added_diag in add_diagnoses_true]
+
+        row['added_diagnoses'] = add_diagnoses_true
+        row['added_diagnoses_grouper_relevant'] = [is_grouper_relevant_diagnoses_revised[idx] for idx in idx_add_diagnoses]
 
         original_procedures = row['all_procedures_original']
         revised_procedures = row['all_procedures_revised']
@@ -63,24 +68,44 @@ def extract_revisions():
         revised_procedures = [p.split(':')[0] for p in revised_procedures]
         revised_procedures = [p for p in revised_procedures if p != '' and p != 'nan']
 
-        added_procedures = tuple(set(revised_procedures).difference(set(original_procedures)))
-        row['added_procedures'] = added_procedures
-        idx_add_procedures = [revised_procedures.index(added_proc) for added_proc in added_procedures]
+        diff_procedures = tuple(set(revised_procedures).difference(set(original_procedures)))
         is_grouper_relevant_procedure_revised = row['is_grouper_relevant_procedures_revised']
-        assert len(is_grouper_relevant_diagnoses_revised) == len(revised_diagnoses), 'Case has deleted procedures'
-        row['added_procedure_grouper_relevant'] = set(
-            [is_grouper_relevant_procedure_revised[idx] for idx in idx_add_procedures])
+
+        add_procedure_true = [proc for proc in diff_procedures if proc not in original_procedures]
+
+        if len(diff_procedures) != len(add_procedure_true):
+            case_id = row['case_id']
+            logger.info(f'Case {case_id} has deleted procedure')
+
+        idx_add_procedures = [revised_procedures.index(added_proc) for added_proc in add_procedure_true]
+
+        row['added_procedures'] = add_procedure_true
+        row['added_procedure_grouper_relevant'] = [is_grouper_relevant_procedure_revised[idx] for idx in idx_add_procedures]
 
         return row
 
     logger.info('Extracting the list of added diagnosis and procedure codes ...')
     df = df.apply(_extract_revision, axis=1)
+    # Analyse if the added procedure grouper relevant or not
+    add_chops = df['added_procedures'].values
+    add_chops_grouper = df['added_procedure_grouper_relevant'].values
+    idx_add_chops_true = [i for i in range(0, len(add_chops)) if len(add_chops[i]) > 0]
+    add_chops_true = add_chops[idx_add_chops_true]
+    add_chops_true_grouper = add_chops_grouper[idx_add_chops_true]
+    add_chops_true_unpacked = list(itertools.chain(*add_chops_true))
+    add_chops_true_grouper_unpacked = list(itertools.chain(*add_chops_true_grouper))
+
+    df_chop_drg_relevant = pd.DataFrame([add_chops_true_unpacked, add_chops_true_grouper_unpacked]).transpose()
+    df_chop_drg_relevant.columns = ['added_procedure', 'is_grouper_relevant']
+    df_chop_drg_relevant_count = df_chop_drg_relevant.groupby('added_procedure')['is_grouper_relevant'].value_counts().unstack(fill_value=0).reset_index()
 
     # save csv with all info : can be used for analysis drg relevant codes before or after revision
     output_path_revised_all_info = os.path.join(ROOT_DIR, 'resources', 'db_data', 'revised_case_all_info.csv')
+    output_path_revised_drg_relevance = os.path.join(ROOT_DIR, 'resources', 'db_data', 'added_procedure_drg_relevance.csv')
     os.makedirs(os.path.dirname(output_path_revised_all_info), exist_ok=True)
     logger.info(f"Storing {df.shape[0]} cases at {output_path_revised_all_info} ...")
     df.to_csv(output_path_revised_all_info, index=False)
+    df_chop_drg_relevant_count.to_csv(output_path_revised_drg_relevance, index=False)
 
     df.drop(columns=['reviewed_original', 'revised_original', 'revision_date_original', 'reviewed_revised',
                      'revised_revised', 'revision_date_revised', 'revision_id_original', 'revision_id_revised',
